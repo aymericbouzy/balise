182451899da99b0f7bf8b4defafd8c876426913a
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-07 14:15:49 +0200

    added 4 new methods

--- model/binet.php
+++ model/binet.php
@@ -1,19 +1,28 @@
 <?php
 
   function create_binet($name, $admin) {
-    $req = Database::get()->prepare("INSERT INTO binet(name) VALUES(':name')");
+    $sql = "INSERT INTO binet(name)
+            VALUES(':name')";
+    $req = Database::get()->prepare($sql);
     $req->execute(array('name' => $name));
     $binet = $req->fetch(PDO::FETCH_ASSOC);
-    $req = Database::get()->prepare("INSERT INTO binet_admin(binet, student, validated_by) VALUES(:binet, :admin, :validated_by)");
+
+    $sql = "INSERT INTO binet_admin(binet, student, validated_by)
+            VALUES(:binet, :admin, :validated_by)";
+    $req = Database::get()->prepare($sql);
     $req->bindParam(':binet', $binet["id"], PDO::PARAM_INT);
     $req->bindParam(':admin', $admin, PDO::PARAM_INT);
     $req->bindParam(':validated_by', $_SESSION["student"], PDO::PARAM_INT);
     $req->execute();
-    return $binet["id"]
+    return $binet["id"];
   }
 
   function set_subsidy_provider($binet, $subsidy_steps) {
-    $req = Database::get()->prepare("UPDATE binet SET subsidy_provider = 1, subsidy_steps = :subsidy_steps WHERE id = :binet LIMIT 1");
+    $sql = "UPDATE binet
+            SET subsidy_provider = 1, subsidy_steps = :subsidy_steps
+            WHERE id = :binet
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
     $req->bindParam(':binet', $binet, PDO::PARAM_INT);
     $req->execute(array(':subsidy_steps' => $subsidy_steps));
   }
@@ -52,5 +61,48 @@
   }
 
   function get_status_admin_binet($binet) {
-    
+    $sql = "SELECT validated_by
+            FROM binet_admin
+            WHERE binet = :binet AND student = :student
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->bindParam(':student', $_SESSION["student"], PDO::PARAM_INT);
+    $req->execute();
+    if ($row = $req->fetch()) {
+      return $row["validated_by"]) or -1;
+    } else {
+      return -2;
+    }
+  }
+
+  function remove_admin_binet($binet, $student) {
+    $sql = "DELETE
+            FROM binet_admin
+            WHERE binet = :binet AND student = :student
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->bindParam(':student', $student, PDO::PARAM_INT);
+    $req->execute();
+  }
+
+  function deactivate_binet($binet) {
+    $sql = "UPDATE binet
+            SET active = 0
+            WHERE id = :binet
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->execute();
+  }
+
+  function activate_binet($binet) {
+    $sql = "UPDATE binet
+            SET active = 1
+            WHERE id = :binet
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->execute();
   }
