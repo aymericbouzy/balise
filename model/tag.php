cf83079362167ccf17c2746644da9dcffb92558e
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-07 18:33:57 +0200

    added deactivate_tag

--- model/tag.php
+++ model/tag.php
@@ -11,3 +11,13 @@
     $tag = $req->fetch(PDO::FETCH_ASSOC);
     return $tag["id"];
   }
+
+  function deactivate_tag($tag) {
+    $sql = "UPDATE tag
+            SET state = 0
+            WHERE id = :tag
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':tag', $tag, PDO::PARAM_INT);
+    $req->execute();
+  }
