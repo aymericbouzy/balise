eae21ce025034588a6f951dbefb36b32a3f913d7
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-07 10:28:32 +0200

    added new binet admin validation

--- model/binet.php
+++ model/binet.php
@@ -7,7 +7,7 @@
     $req = Database::get()->prepare("INSERT INTO binet_admin(binet, student, validated_by) VALUES(:binet, :admin, :validated_by)");
     $req->bindParam(':binet', $binet["id"], PDO::PARAM_INT);
     $req->bindParam(':admin', $admin, PDO::PARAM_INT);
-    $req->bindParam(':validated_by', $_SESSION["student_id"], PDO::PARAM_INT);
+    $req->bindParam(':validated_by', $_SESSION["student"], PDO::PARAM_INT);
     $req->execute();
   }
 
@@ -17,7 +17,7 @@
     $req->execute(array(':subsidy_steps' => $subsidy_steps));
   }
 
-  function get_members($binet) {
+  function get_admins($binet) {
     $sql = "SELECT *
             FROM student
             INNER JOIN binet_admin
@@ -27,4 +27,16 @@
     $req->bindParam(':binet', $binet, PDO::PARAM_INT);
     $req->execute();
     return $req->fetchAll();
+  }
+
+  function validate_admin_binet($student, $binet) {
+    $sql = "UPDATE binet_admin
+            SET validated_by = :validated_by
+            WHERE binet_admin.binet = :binet AND binet.admin.student = :student
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->bindParam(':student', $student, PDO::PARAM_INT);
+    $req->bindParam(':validated_by', $_SESSION["student"], PDO::PARAM_INT);
+    $req->execute();
   }
