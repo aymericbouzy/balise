16e549e349c3199f58d622b92bde929459a7a0a1
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-08 09:09:13 +0200

    add add_tag_spending

--- model/tag.php
+++ model/tag.php
@@ -21,3 +21,22 @@
     $req->bindParam(':tag', $tag, PDO::PARAM_INT);
     $req->execute();
   }
+
+  function reactivate_tag($tag) {
+    $sql = "UPDATE tag
+            SET state = 1
+            WHERE id = :tag
+            LIMIT 1";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':tag', $tag, PDO::PARAM_INT);
+    $req->execute();
+  }
+
+  function add_tag_spending($tag, $spending) {
+    $sql = "INSERT INTO spending_tag(spending, tag)
+            VALUES(:spending, :tag)";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':spending', $spending, PDO::PARAM_INT);
+    $req->bindParam(':tag', $tag, PDO::PARAM_INT);
+    $req->execute();
+  }
