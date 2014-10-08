948d66fea917687aed9b3bb2d5bc5d37aa3d6dcb
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-08 09:46:17 +0200

    added create_subsidy

--- model/subsidy.php
+++ model/subsidy.php
@@ -1,3 +1,16 @@
 <?php
 
-  function create_subsidy($binet, )
+  function create_subsidy($origin, $beneficiary, $amount, $expiration_date = NULL, $purpose = "") {
+    $sql = "INSERT INTO subsidy(origin, beneficiary, amount, expiration_date, purpose)
+            VALUES(:origin, :beneficiary, :amount, :expiration_date, :purpose)";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':origin', $origin, PDO::PARAM_INT);
+    $req->bindParam(':beneficiary', $beneficiary, PDO::PARAM_INT);
+    $req->bindParam(':amount', $amount, PDO::PARAM_INT);
+    $req->execute(array(
+      ':purpose' => $purpose,
+      ':expirationdate' => $expiration_date
+    ));
+    $subsidy = $req->fetch(PDO::FETCH_ASSOC);
+    return $subsidy["id"];
+  }
