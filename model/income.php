0f5c2a2553d2cc4e883eba9a1259ef541eee7467
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-09 17:14:07 +0200

    added create_income

--- model/income.php
+++ model/income.php
@@ -1,1 +1,18 @@
 <?php
+
+  function create_income($amount, $binet, $type, $comment) {
+    $sql = "INSERT INTO income(date, amount, type, created_by, comment)
+            VALUES(CURDATE(), :amount, :binet, :type, :student, :comment)";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':amount', $amount, PDO::PARAM_INT);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->bindParam(':type', $type, PDO::PARAM_INT);
+    $req->bindParam(':student', $_SESSION["student"], PDO::PARAM_INT);
+    $req->execute(array(
+      ':comment' => $comment
+    ));
+    $income = $req->fetch(PDO::FETCH_ASSOC);
+    return $income["id"];
+  }
+
+  
--- model/subsidy.php
+++ model/subsidy.php
@@ -1,13 +1,13 @@
 <?php
 
   function create_subsidy($origin, $beneficiary, $amount, $expiration_date = NULL, $purpose = "") {
-    $sql = "INSERT INTO subsidy(origin, beneficiary, amount, expiration_date, purpose, created_by, validation_by)
-            VALUES(:origin, :beneficiary, :amount, :expiration_date, :purpose, :created_by, NULL)";
+    $sql = "INSERT INTO subsidy(origin, beneficiary, amount, expiration_date, purpose, created_by)
+            VALUES(:origin, :beneficiary, :amount, :expiration_date, :purpose, :student)";
     $req = Database::get()->prepare($sql);
     $req->bindParam(':origin', $origin, PDO::PARAM_INT);
     $req->bindParam(':beneficiary', $beneficiary, PDO::PARAM_INT);
     $req->bindParam(':amount', $amount, PDO::PARAM_INT);
-    $req->bindParam(':created_by', $_SESSION["student"], PDO::PARAM_INT);
+    $req->bindParam(':student', $_SESSION["student"], PDO::PARAM_INT);
     $req->execute(array(
       ':purpose' => $purpose,
       ':expirationdate' => $expiration_date
