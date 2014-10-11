22fd1b4a91e54b4285217189c8c854450e7be87f
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-12 01:56:20 +0200

    finished select_entries

--- model/global.php
+++ model/global.php
@@ -13,6 +13,14 @@
             FROM ".$table."
             WHERE true";
     foreach ($criteria as $column => $value) {
+      if ($column === "tags") {
+        $i = 0;
+        foreach($value as $tag) {
+          $sql .= " AND EXISTS (SELECT * FROM ".$table."_tag WHERE ".$table."_tag.".$table." = ".$table.".id AND ".$table."_tag.tag = :tag".$i.")";
+          $tags[$i] = $tag;
+          $i++;
+        }
+      }
       if (in_array($column, $selectable_int_fields + $selectable_str_fields)) {
         $sql .= " AND ".$column;
         if (is_array($value)) {
@@ -22,23 +30,26 @@
         }
         $sql .= " :".$column;
       }
-      if ($column === "tags") {
-        
-      }
     }
     $req = Database::get()->prepare($sql);
     foreach ($criteria as $column => $value) {
-      if (is_array($value)) {
-        $value = $value[1];
-      }
-      if (is_null($value)) {
-        $req->bindParam(':'.$column, NULL, PDO::PARAM_NULL);
-      } elseif (in_array($column, $selectable_int_fields)) {
-        $req->bindParam(':'.$column, $value, PDO::PARAM_INT);
-      } elseif (in_array($column, $selectable_str_fields)) {
-        $req->bindParam(':'.$column, $value, PDO::PARAM_STR);
+      if ($column === "tags") {
+        for ($j = 0; $j < $i, $j++) {
+          $req->bindParam(':tag'.$j, $tags[$j], PDO::PARAM_INT);
+        }
+      } else {
+        if (is_array($value)) {
+          $value = $value[1];
+        }
+        if (is_null($value)) {
+          $req->bindParam(':'.$column, NULL, PDO::PARAM_NULL);
+        } elseif (in_array($column, $selectable_int_fields)) {
+          $req->bindParam(':'.$column, $value, PDO::PARAM_INT);
+        } elseif (in_array($column, $selectable_str_fields)) {
+          $req->bindParam(':'.$column, $value, PDO::PARAM_STR);
+        }
       }
     }
     $req->execute();
-    return $req->fetch(PDO::FETCH_ASSOC);
+    return $req->fetchAll();
   }
