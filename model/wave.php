95bd64124d4a3f28dfd36be5bfdbef798dec2b2c
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-12 00:39:58 +0200

    added create_wave

--- model/wave.php
+++ model/wave.php
@@ -1,3 +1,15 @@
 <?php
 
-  
+  function create_wave ($binet, $submission_date, $expiry_date) {
+    $sql = "INSERT INTO wave(binet, submission_date, expiry_date)
+            VALUES(:binet, :submission_date, :expiry_date)";
+    $req = Database::get()->prepare($sql);
+    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
+    $req->execute(array(
+      ':purpose' => $purpose,
+      ':submission_date' => $submission_date,
+      ':expiry_date' => $expiry_date
+    ));
+    $wave = $req->fetch(PDO::FETCH_ASSOC);
+    return $wave["id"];
+  }
