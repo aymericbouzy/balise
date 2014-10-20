62a7c6b9cf2f5667b87f1fda25ff1bada97db90c
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-20 15:02:33 +0200

    updated includes

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
   include $GLOBAL_PATH."sql.php";
   include $GLOBAL_PATH."urlrewriting.php";
 
-  include $MODEL_PATH."binet.php";
-  include $MODEL_PATH."spending.php";
-  include $MODEL_PATH."tag.php";
-  include $MODEL_PATH."income.php";
-  include $MODEL_PATH."wave.php";
   include $MODEL_PATH."budget.php";
+  include $MODEL_PATH."tag.php";
+  include $MODEL_PATH."operation.php";
+  include $MODEL_PATH."wave.php";
   include $MODEL_PATH."subsidy.php"; // depends on budget.php
+  include $MODEL_PATH."binet.php"; // depends on budget.php
