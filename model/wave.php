0ec0b0353c0d6e66921e90f001999bb0b7ecf749
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-20 12:05:02 +0200

    updated create_wave

--- model/wave.php
+++ model/wave.php
@@ -1,17 +1,16 @@
 <?php
 
-  function create_wave($binet, $submission_date, $expiry_date) {
-    $sql = "INSERT INTO wave(binet, submission_date, expiry_date)
-            VALUES(:binet, :submission_date, :expiry_date)";
-    $req = Database::get()->prepare($sql);
-    $req->bindParam(':binet', $binet, PDO::PARAM_INT);
-    $req->execute(array(
-      ':purpose' => $purpose,
-      ':submission_date' => $submission_date,
-      ':expiry_date' => $expiry_date
-    ));
-    $wave = $req->fetch(PDO::FETCH_ASSOC);
-    return $wave["id"];
+  function create_wave($binet, $term, $submission_date, $expiry_date) {
+    $values["binet"] = $binet;
+    $values["term"] = $term;
+    $values["submission_date"] = $submission_date;
+    $values["expiry_date"] = $expiry_date;
+    return create_entry(
+      "wave",
+      array("binet", "term"),
+      array("submission_date", "expiry_date"),
+      $values
+    );
   }
 
   function select_wave($wave, $fields = NULL) {
@@ -20,7 +19,7 @@
 
   function select_waves($criteria = array(), $order_by = NULL, $ascending = true) {
     return select_entries("wave",
-                          array("binet", "published"),
+                          array("binet", "term", "published"),
                           array("submission_date", "expiry_date"),
                           $criteria,
                           $order_by,
