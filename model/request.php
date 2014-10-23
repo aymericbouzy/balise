4a49ceb3e8e2b0e4f1d0aa3bf72c794e88bbef19
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-23 16:50:30 +0200

    added select_request - update_request

--- model/request.php
+++ model/request.php
@@ -1,16 +1,28 @@
 <?php
 
-    function create_request($wave, $subsidies, $answer = "") {
-      $values["wave"] = $wave;
-      $values["answer"] = $answer;
-      $request = create_entry(
-        "request",
-        array("wave"),
-        array("answer"),
-        $values
-      );
-      foreach($subsidies as $subsidy) {
-        create_subsidy($subsidy["budget"], $request, $subsidy["amount"], $subsidy["optionnal_values"]);
-      }
-      return $request;
+  function create_request($wave, $subsidies, $answer = "") {
+    $values["wave"] = $wave;
+    $values["answer"] = $answer;
+    $request = create_entry(
+      "request",
+      array("wave"),
+      array("answer"),
+      $values
+    );
+    foreach($subsidies as $subsidy) {
+      create_subsidy($subsidy["budget"], $request, $subsidy["amount"], $subsidy["optionnal_values"]);
     }
+    return $request;
+  }
+
+  function select_request($request, $fields = NULL) {
+    return select_entry("request", $request, $fields);
+  }
+
+  function update_request($request, $hash) {
+    update_entry("request",
+                  array(),
+                  array("answer"),
+                  $request,
+                  $hash);
+  }
