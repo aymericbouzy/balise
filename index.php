5ecc6a0c69604c982dc3539ff759ad8b97de5525
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-23 17:46:35 +0200

    changed order of includes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   include $MODEL_PATH."budget.php";
   include $MODEL_PATH."tag.php";
   include $MODEL_PATH."operation.php";
-  include $MODEL_PATH."request.php";
   include $MODEL_PATH."subsidy.php"; // depends on budget.php
+  include $MODEL_PATH."request.php"; // depends on subsidy.php
   include $MODEL_PATH."binet.php"; // depends on budget.php
   include $MODEL_PATH."wave.php";
