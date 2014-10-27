954340c6e5e752070c253ec842b02f3c95161132
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-28 00:54:57 +0100

    created scaphander

--- global/urlrewriting.php
+++ global/urlrewriting.php
@@ -4,68 +4,17 @@
     return $string;
   }
 
-  function login_path() {
-    return "login";
+  function path($action = "", $model_name = "", $model_id = "", $prefix = "") {
+    return $prefix.(empty($model_name) ? "" : "/".$model_name).(empty($model_id) ? "" : "/".$model_id).(empty($action) ? "" : "/".$action);
   }
 
-  function logout_path() {
-    return "logout";
-  }
-
-  function binet_index_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"];
-  }
-
-  function binet_budget_index_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget";
-  }
-
-  function binet_budget_show_path($binet, $budget) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/".$budget;
-  }
-
-  function binet_budget_new_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/new";
-  }
-
-  function binet_budget_create_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/create";
-  }
-
-  function binet_budget_edit_path($binet, $budget) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/".$budget."/edit";
-  }
-
-  function binet_budget_update_path($binet, $budget) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/".$budget."/update";
-  }
-
-  function binet_budget_delete_path($binet, $budget) {
-    return "binet/".select_binet($binet)["clean_name"]."/budget/".$budget."/delete";
-  }
-
-  function binet_operation_index_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation";
-  }
-
-  function binet_operation_show_path($binet, $operation) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation/".$operation;
-  }
-
-  function binet_operation_new_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation/new";
-  }
-
-  function binet_operation_create_path($binet) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation/create";
-  }
-
-  function binet_operation_edit_path($binet, $operation) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation/".$operation."/edit";
-  }
-
-  function binet_operation_update_path($binet, $operation) {
-    return "binet/".select_binet($binet)["clean_name"]."/operation/".$operation."/update";
+  function write_scaphander_rules($htaccess, $model_name, $binet_prefix = false, $collection_actions = array("index", "new", "create"), $member_actions = array("show", "edit", "update", "delete")) {
+    foreach ($collection_actions as $action) {
+      write_path_rule($htaccess, path($action, $model_name, "", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&");
+    }
+    foreach ($member_actions as $action) {
+      write_path_rule($htaccess, path($action, $model_name, "([0-9]+)", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&".$model_name."=$".($binet_prefix ? "3" : "1")."&");
+    }
   }
 
   function write_path_rule($htaccess, $path, $url) {
@@ -80,29 +29,24 @@
   	if (!$htaccess) {
   		echo ".htaccess could not be opened for urlrewriting.";
   		exit;
-  	} else {
-  		if (fwrite($htaccess, "ErrorDocument  404  ./?id=404
-  	                         AddDefaultCharset iso-8859-1
-  	                         RewriteEngine on
-  	                        ") === FALSE) {
-	       echo ".htaccess could not be written for urlrewriting.";
-	       exit;
-	    }
-
-      write_path_rule($htaccess, login_path(), "frankiz.php?action=login&");
-      write_path_rule($htaccess, logout_path(), "frankiz.php?action=logout&");
-
-      foreach(select_binets() as $binet) {
-        write_path_rule($htaccess, binet_index_path($binet["id"]), "binet/budget.php?action=index&binet=".$binet["id"]."&");
-        write_path_rule($htaccess, binet_budget_index_path($binet["id"]), "binet/budget.php?action=index&binet=".$binet["id"]."&");
-        foreach (select_budgets(array("binet" => $binet["id"])) as $budget) {
-          write_path_rule($htaccess, binet_budget_show_path($binet["id"], $budget["id"]), "binet/budget.php?action=index&binet=".$binet["id"]."&budget=".$budget["id"]."&");
-          write_path_rule($htaccess, binet_budget_edit_path($binet["id"], $budget["id"]), "binet/budget.php?action=edit&binet=".$binet["id"]."&budget=".$budget["id"]."&");
-          write_path_rule($htaccess, binet_budget_update_path($binet["id"], $budget["id"]), "binet/budget.php?action=update&binet=".$binet["id"]."&budget=".$budget["id"]."&");
-          write_path_rule($htaccess, binet_budget_delete_path($binet["id"], $budget["id"]), "binet/budget.php?action=delete&binet=".$binet["id"]."&budget=".$budget["id"]."&");
-        }
-        write_path_rule($htaccess, binet_budget_new_path($binet["id"]), "binet/budget.php?action=new&binet=".$binet["id"]."&");
-        write_path_rule($htaccess, binet_budget_create_path($binet["id"]), "binet/budget.php?action=create&binet=".$binet["id"]."&");
-      }
+  	}
+		if (fwrite($htaccess, "ErrorDocument  404  ./?id=404
+	                         AddDefaultCharset iso-8859-1
+	                         RewriteEngine on
+	                        ") === FALSE) {
+       echo ".htaccess could not be written for urlrewriting.";
+       exit;
     }
+
+    write_path_rule($htaccess, path("login"), "frankiz.php?action=login&");
+    write_path_rule($htaccess, path("logout"), "frankiz.php?action=logout&");
+
+    write_path_rule($htaccess, path("", "binet", "([a-z-]+)/([0-9]+)"), "binet/budget.php?action=index&binet=$1&term=$2&");
+    write_scaphander_rules($htaccess, "budget", true);
+    write_scaphander_rules($htaccess, "operation", true);
+    write_scaphander_rules($htaccess, "request", true);
+    write_scaphander_rules($htaccess, "tag", false, array("index", "create"), array("show"));
+    write_scaphander_rules($htaccess, "binet", false, array("index", "new", "create"), array("show", "edit", "update"));
+    write_scaphander_rules($htaccess, "wave", false, array("index"), array("show"));
+    write_scaphander_rules($htaccess, "wave", true, array("index", "new", "create"), array("show", "edit", "update"));
   }
