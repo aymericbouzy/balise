e127f894e52a4ddf10b76e351309c747842daeb0
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-28 10:05:20 +0100

    added scaphander rule for binet - added root to index

--- global/urlrewriting.php
+++ global/urlrewriting.php
@@ -16,12 +16,27 @@
     }
   }
 
-  function write_scaphander_rules($htaccess, $model_name, $binet_prefix = false, $collection_actions = array("index", "new", "create"), $member_actions = array("show", "edit", "update", "delete")) {
+  function write_scaphander_rules($htaccess, $model_name, $binet_prefix = false, $collection_actions = array("new", "create"), $member_actions = array("show", "edit", "update", "delete"), $model_id = "([0-9]+)", $model_query_string = "") {
+    write_path_rule(
+      $htaccess,
+      path("", $model_name, "", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""),
+      ($binet_prefix ? "binet/").$model_name.".php?action=index".($binet_prefix ? "&binet=$1&term=$2" : "")."&"
+    );
+    $collection_actions[] = "index";
     foreach ($collection_actions as $action) {
-      write_path_rule($htaccess, path($action, $model_name, "", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&");
+      write_path_rule(
+        $htaccess,
+        path($action, $model_name, "", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""),
+        ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&"
+      );
     }
+    $model_query_string = empty($model_query_string) ? $model_name."=$".($binet_prefix ? "3" : "1") : $model_query_string;
     foreach ($member_actions as $action) {
-      write_path_rule($htaccess, path($action, $model_name, "([0-9]+)", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&".$model_name."=$".($binet_prefix ? "3" : "1")."&");
+      write_path_rule(
+        $htaccess,
+        path($action, $model_name, $model_id, $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""),
+        ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&".$model_query_string."&"
+      );
     }
   }
 
@@ -43,12 +58,12 @@
     write_path_rule($htaccess, path("login"), "frankiz.php?action=login&");
     write_path_rule($htaccess, path("logout"), "frankiz.php?action=logout&");
 
-    write_path_rule($htaccess, path("", "binet", "([a-z-]+)/([0-9]+)"), "binet/budget.php?action=index&binet=$1&term=$2&");
+    write_scaphander_rules($htaccess, "binet", false, array("new", "create"), array("show", "edit", "update", "set_subsidy_provider"), "([a-z-]+)/([0-9]+)", "binet=$1&term=$2");
     write_scaphander_rules($htaccess, "budget", true);
-    write_scaphander_rules($htaccess, "operation", true, array("index", "new", "create"), array("show", "edit", "update", "delete", "validate"));
+    write_scaphander_rules($htaccess, "operation", true, array("new", "create"), array("show", "edit", "update", "delete", "validate"));
     write_scaphander_rules($htaccess, "request", true);
-    write_scaphander_rules($htaccess, "tag", false, array("index", "create"), array("show"));
-    write_scaphander_rules($htaccess, "binet", false, array("index", "new", "create"), array("show", "edit", "update"));
-    write_scaphander_rules($htaccess, "wave", false, array("index"), array("show"));
-    write_scaphander_rules($htaccess, "wave", true, array("index", "new", "create"), array("show", "edit", "update"));
+    write_scaphander_rules($htaccess, "tag", false, array("create"), array("show"));
+    write_scaphander_rules($htaccess, "binet", false, array("new", "create"), array("show", "edit", "update"));
+    write_scaphander_rules($htaccess, "wave", false, array(), array("show"));
+    write_scaphander_rules($htaccess, "wave", true, array("new", "create"), array("show", "edit", "update"));
   }
