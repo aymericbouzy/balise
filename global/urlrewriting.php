1c0bb191a7a99f9f8999d5d7695f208ec6357c2d
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-28 01:06:25 +0100

    added validate action to operation

--- global/urlrewriting.php
+++ global/urlrewriting.php
@@ -1,5 +1,6 @@
 <?php
 
+  // TODO: implement function
   function clean_string($string) {
     return $string;
   }
@@ -8,19 +9,19 @@
     return $prefix.(empty($model_name) ? "" : "/".$model_name).(empty($model_id) ? "" : "/".$model_id).(empty($action) ? "" : "/".$action);
   }
 
+  function write_path_rule($htaccess, $path, $url) {
+    if (fwrite($htaccess, "RewriteRule ".$path." ./controller/".$url."%{QUERY_STRING} [L]
+    ") === FALSE && $_ENV["development"]) {
+      echo ".htaccess could not be written for urlrewriting.";
+    }
+  }
+
   function write_scaphander_rules($htaccess, $model_name, $binet_prefix = false, $collection_actions = array("index", "new", "create"), $member_actions = array("show", "edit", "update", "delete")) {
     foreach ($collection_actions as $action) {
       write_path_rule($htaccess, path($action, $model_name, "", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&");
     }
     foreach ($member_actions as $action) {
       write_path_rule($htaccess, path($action, $model_name, "([0-9]+)", $binet_prefix ? "binet/([a-z-]+)/([0-9]+)/" : ""), ($binet_prefix ? "binet/").$model_name.".php?action=".$action.($binet_prefix ? "&binet=$1&term=$2" : "")."&".$model_name."=$".($binet_prefix ? "3" : "1")."&");
-    }
-  }
-
-  function write_path_rule($htaccess, $path, $url) {
-    if (fwrite($htaccess, "RewriteRule ".$path." ./controller/".$url."%{QUERY_STRING} [L]
-    ") === FALSE && $_ENV["development"]) {
-      echo ".htaccess could not be written for urlrewriting.";
     }
   }
 
@@ -38,12 +39,13 @@
        exit;
     }
 
+    write_path_rule($htaccess, path(), "frankiz.php?action=login&");
     write_path_rule($htaccess, path("login"), "frankiz.php?action=login&");
     write_path_rule($htaccess, path("logout"), "frankiz.php?action=logout&");
 
     write_path_rule($htaccess, path("", "binet", "([a-z-]+)/([0-9]+)"), "binet/budget.php?action=index&binet=$1&term=$2&");
     write_scaphander_rules($htaccess, "budget", true);
-    write_scaphander_rules($htaccess, "operation", true);
+    write_scaphander_rules($htaccess, "operation", true, array("index", "new", "create"), array("show", "edit", "update", "delete", "validate"));
     write_scaphander_rules($htaccess, "request", true);
     write_scaphander_rules($htaccess, "tag", false, array("index", "create"), array("show"));
     write_scaphander_rules($htaccess, "binet", false, array("index", "new", "create"), array("show", "edit", "update"));
