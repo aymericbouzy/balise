a893810fc1838ece1d368915a9068e20dd88cc14
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-10-29 13:07:47 +0100

    corrections

--- controller/base.php
+++ controller/base.php
@@ -1,5 +1,6 @@
 <?php
 
+  include "../global/initialisation.php";
   correct_action();
   if (!validate_input(array("student"), "session") && $_GET["action"] != "login") {
     header("Location: ".$SCHEME."://".$HOST."/".path("login"));
--- controller/binet.php
+++ controller/binet.php
@@ -1,28 +1,41 @@
 <?php
 
   include "base.php";
-  before_action($correct_binet_term, array("edit", "update", "set_subsidy_provider", "show", "change_term", "deactivate"));
-
-  before_action($kessier, array("new", "create", "change_term", "deactivate", "set_subsidy_provider"));
-  before_action($member_binet_term, array("edit", "update"));
+  
+  before_action("correct_binet_term", array("edit", "update", "set_subsidy_provider", "show", "change_term", "deactivate"));
+  before_action("kessier", array("new", "create", "change_term", "deactivate", "set_subsidy_provider"));
+  before_action("member_binet_term", array("edit", "update"));
 
   switch ($_GET["action"]) {
+
   case "index":
     break;
+
   case "new":
     break;
+
   case "create":
     break;
+
   case "edit":
     break;
+
   case "update":
     break;
+
   case "set_subsidy_provider":
     break;
+
   case "show":
     break;
+
   case "change_term":
     break;
+
   case "deactivate":
     break;
+
+  default:
+    header_if(true, 403);
+    exit;
   }
