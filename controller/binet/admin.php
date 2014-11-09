94f2e2967381c124cd26431b4939cf10bded587a
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-11-09 02:32:10 +0100

    corrections

--- controller/binet/admin.php
+++ controller/binet/admin.php
@@ -12,12 +12,12 @@
     break;
 
   case "create":
-    $_SESSION["notice"] = $student["full_name"]." est à présent administrateur du binet ".$binet["name"]." pour le mandat ".$term.".";
+    $_SESSION["notice"] = $admin["full_name"]." est à présent administrateur du binet ".$binet["name"]." pour le mandat ".$term.".";
     redirect_to(array("action" => ""));
     break;
 
   case "delete":
-    $_SESSION["notice"] = "Les droits d'administration de "$student["full_name"]." pour le mandat ".$term." du binet ".$binet["name"]." ont été révoqués.";
+    $_SESSION["notice"] = "Les droits d'administration de ".$admin["full_name"]." pour le mandat ".$term." du binet ".$binet["name"]." ont été révoqués.";
     redirect_to(array("action" => ""));
     break;
 
