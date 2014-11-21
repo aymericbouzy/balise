fc257e7502205c37d03253ac3975336d49924160
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-11-21 14:44:30 +0100

    sidebar updated with Victor's code

--- htdocs/view/layout/structure.php
+++ htdocs/view/layout/structure.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <?php
     include $LAYOUT_PATH."header.php";
-    include $LAYOUT_PATH."aside.php";
+    include $LAYOUT_PATH."sidebar.php";
   ?>
 </nav>
