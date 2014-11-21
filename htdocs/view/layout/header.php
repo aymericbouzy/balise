37e3a2f44a1e30740cd29e00a5fe69a0b3a36dca
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-11-21 14:09:40 +0100

    header updated with Victor's code

--- htdocs/view/layout/header.php
+++ htdocs/view/layout/header.php
@@ -12,101 +12,65 @@
   <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">#nomDuBinet</a>
     <ul class="dropdown-menu" role="menu">
-      <li><a href="#">Binet massage</a></li>
-      <li><a href="#">X-Circus</a></li>
-      <li><a href="#">JTX</a></li>
+      <?php foreach(binet_admins_current_student() as $binet_admin) {
+        $binet_admin["binet_name"] = select_binet($binet_admin["binet"], array("name"))["name"];
+        ?>
+        <li>
+          <?php echo link_to(path("", "binet", binet_term_id($binet_admin["binet"], $binet_admin["term"])), $binet_admin["binet_name"]."<span class=\"binet-term\">".$binet_admin["term"]."</span>"); ?>
+        </li>
+        <?php
+      }
+      ?>
     </ul>
   </li>
 </ul>
-            <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-plus-circle green-plus" id="operation-plus"></i> <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li class="add-operation">
-                                <a href="#"><i class="fa fa-fw fa-calculator"></i> Opération</a>
-                            </li>
-                            <li>
-                                <a class="add-operation" href="#"><i class="fa fa-fw fa-bar-chart"></i> Ligne budgétaire</a>
-                            </li >
-                            <li class="add-operation">
-                                <a class="add-operation" href="#"><i class="fa fa-fw fa-money"></i> Demande de subvention</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li class="add-operation">
-                                <a class="add-operation" href="#"><i class="fa fa-fw fa-group"></i> Binet</a>
-                            </li>
-                        </ul>
 
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Robert Blanquette <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Autorisations</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-<div id="header-left">
-  <ul>
-    <li>
-      <?php echo link_to(path("", "binet", binet_term_id($binet["id"], $term)), $binet["name"]."<span class=\"binet-term\">".$term."</span>"); ?>
-      <ul>
-        <?php foreach(binet_admins_current_student() as $binet_admin) {
-          $binet_admin["name"] = select_binet($binet_admin["binet"], array("name"))["name"];
-          ?>
-          <li>
-            <?php echo link_to(path("", "binet", binet_term_id($binet_admin["binet"], $binet_admin["term"])), $binet_admin["name"]."<span class=\"binet-term\">".$binet_admin["term"]."</span>"); ?>
-          </li>
-          <?php
-        }
+
+<ul class="nav navbar-right top-nav">
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <i class="fa fa-plus-circle green-plus" id="operation-plus"></i> <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu" role="menu">
+      <li class="add-operation">
+        <?php echo link_to(path("new", "operation", "", binet_prefix($binet["id"], $term)), "<i class=\"fa fa-fw fa-calculator\"></i> Opération", "add-operation"); ?>
+      </li>
+      <li>
+        <?php echo link_to(path("new", "budget", "", binet_prefix($binet["id"], $term)), "<i class=\"fa fa-fw fa-bar-chart\"></i> Ligne budgétaire", "add-operation"); ?>
+      </li >
+      <li class="add-operation">
+        <?php echo link_to(path("new", "request", "", binet_prefix($binet["id"], $term)), "<i class=\"fa fa-fw fa-money\"></i> Demande de subvention", "add-operation"); ?>
+      </li>
+      <?php if (select_binet($binet["id"], array("subsidy_provider"))["subsidy_provider"] == 1) {
         ?>
-      </ul>
-    </li>
-    <li>
-      <i class="fa-plus"></i>
-      <ul>
+        <li class = "divider"></li>
         <li>
-          <?php echo link_to(path("new", "budget", "", binet_prefix($binet["id"], $term)), "Ligne budgétaire"); ?>
+          <?php echo link_to(path("new", "wave", "", binet_prefix($binet["id"], $term)), "<i class=\"fa fa-fw fa-money\"></i> Vague de subvention", "add-operation"); ?>
         </li>
-        <li>
-          <?php echo link_to(path("new", "operation", "", binet_prefix($binet["id"], $term)), "Opération"); ?>
-        </li>
-        <li>
-          <?php echo link_to(path("new", "request", "", binet_prefix($binet["id"], $term)), "Demande de subvention"); ?>
-        </li>
-        <?php if (select_binet($binet["id"], array("subsidy_provider"))["subsidy_provider"] == 1) {
-          ?>
-          <li class = "seperator"></li>
-          <li>
-            <?php echo link_to(path("new", "wave", "", binet_prefix($binet["id"], $term)), "Vague de subvention"); ?>
-          </li>
-          <?php
-        }
-        if ($binet["id"] == $KES_ID) {
-          ?>
-          <li class = "seperator"></li>
-          <li>
-            <?php echo link_to(path("new", "binet"), "Binet"); ?>
-          </li>
-          <?php
-        }
+        <?php
+      }
+      if ($binet["id"] == $KES_ID) {
         ?>
-      </ul>
-    </li>
-  </ul>
-</div>
-<div id="header-center">
-  <?php echo link_to(path(), img("??")) ?>
-</div>
-<div id="header-right">
-  <div id="header-name">
-    <?php echo $current_student["name"]; ?>
-  </div>
-  <?php echo link_to(path("logout", "home"), "<i class=\"fa-logout\" alt=\"logout\"></i>") ?>
-</div>
+        <li class = "divider"></li>
+        <li>
+          <?php echo link_to(path("new", "binet"), "<i class="fa fa-fw fa-group"></i> Binet", "add-operation"); ?>
+        </li>
+        <?php
+      }
+      ?>
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $current_student["name"]; ?>e <b class="caret"></b></a>
+    <ul class="dropdown-menu">
+      <li>
+        <!--TODO-->
+        <a href="#"><i class="fa fa-fw fa-user"></i> Autorisations</a>
+      </li>
+      <li class="divider"></li>
+      <li>
+        <?php echo link_to(path("logout", "home"), "<i class=\"fa fa-fw fa-power-off\"></i> Déconnexion") ?>
+      </li>
+    </ul>
+  </li>
+</ul>
