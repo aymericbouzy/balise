16a9a626b4945c87d70fb3b15ba780cc00db00df
victornicolet <victor.nicolet.0@gmail.com>
2014-12-14 12:08:55 +0100

    New simple login page - bound to change

--- htdocs/view/layout/application.php
+++ htdocs/view/layout/application.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
-  <html lang="">
+  <html lang="fr">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="nicolet" >
     <title>Balise Trézo</title>
-    <link rel="shortcut icon" href="">
-    		<!--Minified Bootstrap CSS-->
-    		<link rel="stylesheet" href="<?php echo $ASSET_PATH; ?>dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo $ASSET_PATH; ?>dist/css/bootstrap-theme.min.css">
-        <!--Switch CSS-->
-        <link rel="stylesheet" href="<?php echo $ASSET_PATH; ?>css/bootstrap-switch.css">
-        <!-- Custom CSS -->
-        <link href="<?php echo $ASSET_PATH; ?>css/user-home.css" rel="stylesheet">
-        <!-- Custom Fonts -->
-        <link href="<?php echo $ASSET_PATH; ?>font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    
+    	<!-- CSS -->
+    	<!--Minified Bootstrap CSS-->
+    	<link rel="stylesheet" href="<?php echo $ASSET_PATH; ?>dist/css/bootstrap.min.css">
+     	<!-- Custom CSS -->
+     	<link rel="stylesheet" href="<?php echo $ASSET_PATH; ?>css/user-home.css">
+      
+     	<!-- Custom Fonts -->
+     	<link href="<?php echo $ASSET_PATH; ?>font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-			<!-- jQuery -->
-        <script src="<?php echo $ASSET_PATH; ?>js/jquery.js"></script>
-        <!--Core Bootstrap JS-->
-        <script src="<?php echo $ASSET_PATH; ?>dist/js/bootstrap.min.js"></script>
-        <!--Switch JS-->
-        <script src="<?php echo $ASSET_PATH; ?>js/bootstrap-switch.js"></script>
+		<!-- JavaScript -->
+		<!-- jQuery -->
+     	<script src="<?php echo $ASSET_PATH; ?>js/jquery.js"></script>
+     	<!--Core Bootstrap JS-->
+     	<script src="<?php echo $ASSET_PATH; ?>dist/js/bootstrap.min.js"></script>
 
-    <!--[if IE]>
-      <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+
+   	<!--[if IE]>
+      	<script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+      	<script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+    
+    
   </head>
   <body>
     <div id="wrapper">
       <?php
-        if ($_GET["controller"] == "error" ) {
+        if ($_GET["controller"] == "error" || $_GET["controller"] == "home" && $_GET["action"] == "welcome") {
 
         } else {
           include $LAYOUT_PATH."structure.php";
