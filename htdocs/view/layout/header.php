90a33354e8882f25e8c369f2d697f3f244a5f55b
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-12-15 10:06:24 +0100

    new operation for a foreign binet when no binet view

--- htdocs/view/layout/header.php
+++ htdocs/view/layout/header.php
@@ -32,31 +32,40 @@
       <i class="fa fa-plus-circle green-plus" id="operation-plus"></i> <span class="caret"></span>
     </a>
     <ul class="dropdown-menu" role="menu">
-      <li class="add-operation">
-        <?php echo link_to(path("new", "operation", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-calculator\"></i> Opération", "add-operation"); ?>
-      </li>
-      <li>
-        <?php echo link_to(path("new", "budget", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-bar-chart\"></i> Ligne budgétaire", "add-operation"); ?>
-      </li >
-      <li class="add-operation">
-        <?php echo link_to(path("new", "request", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Demande de subvention", "add-operation"); ?>
-      </li>
-      <?php if (select_binet($binet, array("subsidy_provider"))["subsidy_provider"] == 1) {
+      <?php if (isset($_GET["prefix"]) && $_GET["prefix"] == "binet") {
         ?>
-        <li class = "divider"></li>
-        <li>
-          <?php echo link_to(path("new", "wave", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Vague de subvention", "add-operation"); ?>
-        </li>
-        <?php
-      }
-      if ($binet == KES_ID) {
-        ?>
-        <li class = "divider"></li>
-        <li>
-          <?php echo link_to(path("new", "binet"), "<i class=\"fa fa-fw fa-group\"></i> Binet", "add-operation"); ?>
-        </li>
-        <?php
-      }
+          <li class="add-operation">
+            <?php echo link_to(path("new", "operation", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-calculator\"></i> Opération", "add-operation"); ?>
+          </li>
+          <li>
+            <?php echo link_to(path("new", "budget", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-bar-chart\"></i> Ligne budgétaire", "add-operation"); ?>
+          </li >
+          <li class="add-operation">
+            <?php echo link_to(path("new", "request", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Demande de subvention", "add-operation"); ?>
+          </li>
+          <?php if (select_binet($binet, array("subsidy_provider"))["subsidy_provider"] == 1) {
+            ?>
+            <li class = "divider"></li>
+            <li>
+              <?php echo link_to(path("new", "wave", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Vague de subvention", "add-operation"); ?>
+            </li>
+            <?php
+          }
+          if ($binet == KES_ID) {
+            ?>
+            <li class = "divider"></li>
+            <li>
+              <?php echo link_to(path("new", "binet"), "<i class=\"fa fa-fw fa-group\"></i> Binet", "add-operation"); ?>
+            </li>
+            <?php
+          }
+        } else {
+          ?>
+            <li>
+              <?php echo link_to(path("new", "operation"), "<i class=\"fa fa-fw fa-calculator\"></i> Opération", "add-operation"); ?>
+            </li>
+          <?php
+        }
       ?>
     </ul>
   </li>
