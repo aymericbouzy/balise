f647c958aad14360ec33593eeafe7ce0b570431f
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-12-17 17:12:17 +0100

    created form pre-fill for request

--- htdocs/controller/binet/request.php
+++ htdocs/controller/binet/request.php
@@ -1,17 +1,30 @@
 <?php
 
   define("amount_prefix", "amount_");
+  define("purpose_prefix", "purpose_");
 
-  function adds_amount_prefix($budget) {
-    return amount_prefix.$budget["id"];
+  function adds_amount_prefix($object) {
+    return amount_prefix.$object["id"];
   }
 
-  function subsidy_amount_requested_array() {
-    return array_map(select_budgets(array("binet" => $GLOBALS["binet"], "term" => $GLOBALS["term"])), "adds_amount_prefix");
+  function adds_purpose_prefix($object) {
+    return purpose_prefix.$object["id"];
+  }
+
+  function budgets_involved() {
+    return select_budgets(array("binet" => $GLOBALS["binet"], "term" => $GLOBALS["term"]));
+  }
+
+  function subsidies_involved() {
+    return select_subsidies(array("request" => $GLOBALS["request"]));
   }
 
   function adds_max_amount($amount) {
     return array($amount, MAX_AMOUNT);
+  }
+
+  function adds_max_length_purpose($purpose) {
+    return array($purpose, 128);
   }
 
   function not_sent() {
@@ -22,15 +35,19 @@
   before_action("check_csrf_get", array("delete", "send"));
   before_action("check_entry", array("show", "edit", "update", "delete", "send"), array("model_name" => "request", "binet" => $binet, "term" => $term));
   before_action("check_editing_rights", array("new", "create", "edit", "update", "delete", "send"));
+  $requested_amount_array = array_map("adds_amount_prefix", budgets_involved());
+  $purpose_array = array_map("adds_purpose_prefix", budgets_involved());
   before_action("check_form_input", array("create", "update"), array(
     "model_name" => "request",
-    "str_fields" => array(array("answer", 100000)),
-    "amount_fields" => array_map("adds_max_amount", subsidy_amount_requested_array()),
-    "other_fields" => array(array("wave", "exists_wave"), array("paid_by", "exists_student")),
+    "str_fields" => array_merge(array(array("answer", 100000)), array_map("adds_max_length_purpose", $purpose_array)),
+    "amount_fields" => array_map("adds_max_amount", $requested_amount_array),
+    "other_fields" => array(array("wave", "exists_wave")),
     "redirect_to" => path($_GET["action"] == "update" ? "edit" : "new", "request", $_GET["action"] == "update" ? $request["id"] : "", binet_prefix($binet, $term))
   ));
   before_action("not_sent", array("send", "edit", "update", "delete"));
   before_action("generate_csrf_token", array("new", "edit", "show"));
+
+  $edit_form_fields = array_merge(array("answer", "wave"), $requested_amount_array, $purpose_array);
 
   switch ($_GET["action"]) {
 
@@ -38,6 +55,12 @@
     break;
 
   case "new":
+    foreach (budgets_involved() as $budget) {
+      $request["budget"][$budget["id"]] = select_budget($budget["id"], array("id", "label", "amount", "real_amount", "subsidized_amount_requested", "subsidized_amount_granted", "subsidized_amount_used"));
+      foreach (select_tags(array("budget" => $budget["id"])) as $tag) {
+        $request["budget"][$budget["id"]]["tags"][] = select_tag($tag["id"], array("id", "name"));
+      }
+    }
     break;
 
   case "create":
@@ -49,6 +72,21 @@
     break;
 
   case "edit":
+    function request_to_form_fields($request) {
+      foreach (budgets_involved() as $budget) {
+        $subsidies = select_subsidies(array("budget" => $budget["id"]));
+        if (empty($subsidies)) {
+          $request[add_amount_prefix($budget)] = 0;
+          $request[add_purpose_prefix($budget)] = "";
+        } else {
+          $subsidy = select_subsidy($subsidies[0]["id"], array("requested_amount", "purpose"));
+          $request[add_amount_prefix($budget)] = $subsidy["requested_amount"];
+          $request[add_purpose_prefix($budget)] = $subsidy["purpose"];
+        }
+      }
+      return $request;
+    }
+    $request = set_editable_entry_for_form("request", $request, $edit_form_fields);
     break;
 
   case "update":
