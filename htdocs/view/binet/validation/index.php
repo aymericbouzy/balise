b9af074b0599d4730a0b42682bbdd21dcf72254a
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2014-12-20 09:52:38 +0100

    indentation and naming

--- htdocs/view/binet/validation/index.php
+++ htdocs/view/binet/validation/index.php
@@ -1,29 +1,69 @@
 <div class="row-centered">
-                    <div class="col-max">
-                        <h2 class="tabtitle">Opérations à valider</h2>
-                        <!--Script links to selected operation when line clicked-->
-                        <script>
-                            function goto(str){
-                                window.location.href=str;
-                            }
-                        </script>
-                            
-                       <div class="table-responsive" id="validations-table">
-                            <table class="table table-bordered table-hover table-small-char">
-                               <thead>
-                                   	<tr>
-                                       <th>Date</th>
-													<th>Nom</th>
-													<th>Origine</th>
-													<th>Montant</th>
-                                    	</tr>
-                                		</thead>
-                               <tbody>
-                                    <?php
-                                    	foreach($validations as $validations){
-													echo line_clickable($date,$title,$origin,$amount,$id) }?>
-                               </tbody>
-                           </table>
-                       </div>
-                    </div>
-                </div>
+  <div class="col-max">
+    <h2 class="tabtitle">Opérations à valider</h2>
+    <!--Script links to selected operation when line clicked-->
+    <script>
+      function goto(str){
+        window.location.href=str;
+      }
+    </script>
+    <div class="table-responsive" id="validations-table">
+      <table class="table table-bordered table-hover table-small-char">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Nom</th>
+            <th>Origine</th>
+            <th>Montant</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            foreach ($pending_validations_operations as $operation) {
+              echo validatable_operation_line($operation, true);
+            }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<?php
+
+  if ($binet == KES_ID) {
+    ?>
+      <div class="row-centered">
+        <div class="col-max">
+          <h2 class="tabtitle">Opérations à valider par la Kès</h2>
+          <!--Script links to selected operation when line clicked-->
+          <script>
+          function goto(str){
+            window.location.href=str;
+          }
+          </script>
+          <div class="table-responsive" id="validations-table-kes">
+            <table class="table table-bordered table-hover table-small-char">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Nom</th>
+                  <th>Origine</th>
+                  <th>Montant</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  foreach ($pending_validations_operations as $operation) {
+                    echo validatable_operation_line($operation, true);
+                  }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    <?php
+  }
+
+?>
