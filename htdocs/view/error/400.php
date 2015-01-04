44e622ec89dd0c28cfc1bd8b0433a86ad1191406
victornicolet <victor.nicolet.0@gmail.com>
2015-01-04 15:28:21 +0100

    Nicer errors

--- htdocs/view/error/400.php
+++ htdocs/view/error/400.php
@@ -1,3 +1,14 @@
-<p>
-  Error 400 : Bad Request
-</p>
+<div id="error" class="container">
+  <div class="row">
+    <div class="col-lg-2">
+    </div>
+    <div class="col-lg-8">
+      <div class="jumbotron">
+        <h1><i class="fa fa-fw fa-frown-o"></i> 400 error</h1>
+        <p>Bad request</p>
+      </div>
+    </div>
+    <div class="col-lg-2">
+    </div>
+  </div>
+</div>
--- htdocs/view/error/404.php
+++ htdocs/view/error/404.php
@@ -1,3 +1,14 @@
-<p>
-  Error 404 : Not Found
-</p>
+<div id="error" class="container">
+  <div class="row">
+    <div class="col-lg-2">
+    </div>
+    <div class="col-lg-8">
+      <div class="jumbotron">
+        <h1><i class="fa fa-fw fa-ambulance"></i> 404 error</h1>
+        <p>Not found</p>
+      </div>
+    </div>
+    <div class="col-lg-2">
+    </div>
+  </div>
+</div>
