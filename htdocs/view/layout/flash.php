ce3475967304204b6d81714ef9f62ae0df164292
victornicolet <victor.nicolet.0@gmail.com>
2015-01-04 15:29:15 +0100

    Flash cards

--- htdocs/view/layout/flash.php
+++ htdocs/view/layout/flash.php
@@ -2,16 +2,33 @@
 
   function print_flash($class) {
     if (!empty($_SESSION[$class])) {
+      $cssClass= "alert-default";
+      if($class== "notice"){
+        $cssClass= "alert-success";
+      }else if($class== "error"){
+        $cssClass= "alert-danger":
+      }
       foreach ($_SESSION[$class] as $flash) {
         ?>
-
-        <?php echo $flash; ?>
-
+        <div class="col-lg-4">
+          <div class="alert <?php echo $cssClass ?> alert-dismissible fade in" role="alert">
+            <button type="button" class="close" data-dismiss="alert">
+              <span aria-hidden="true">×</span><span class="sr-only">Fermer</span></button>
+              <strong>
+                <?php echo $flash; ?>
+              </strong>
+          </div>
+        </div>
         <?php
       }
       unset($_SESSION[$class]);
     }
   }
+?>
 
-  print_flash("notice");
-  print_flash("error");
+  <div class="row">
+    <?php
+      print_flash("notice");
+      print_flash("error");
+    ?>
+  </div>
