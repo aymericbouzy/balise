13083507b830153b28ac95bb0a895cedf78649c7
victornicolet <victor.nicolet.0@gmail.com>
2015-01-10 17:43:42 +0100

    Forms layout

--- htdocs/view/binet/request/edit.php
+++ htdocs/view/binet/request/edit.php
@@ -1,6 +1,8 @@
-<h1>Modifier la demande de subvention</h1>
-<?php
-  $form_action = "update";
-  $submit_label = "Sauvegarder";
-  include "edit_form.php";
-?>
+<div class="form-container">
+  <h1><i class="fa fa-fw fa-edit"></i> Modifier la demande de subvention</h1>
+  <?php
+    $form_action = "update";
+    $submit_label = "Sauvegarder";
+    include "edit_form.php";
+  ?>
+</div>
--- htdocs/view/binet/wave/edit.php
+++ htdocs/view/binet/wave/edit.php
@@ -1,6 +1,8 @@
-<h1>Modifier la vague de subvention</h1>
-<?php
-  $form_action = "update";
-  $submit_label = "Mettre à jour";
-  include "form.php";
-?>
+<div class="form-container">
+  <h1><i class="fa fa-fw fa-edit"></i> Modifier la vague de subvention</h1>
+  <?php
+    $form_action = "update";
+    $submit_label = "Mettre à jour";
+    include "form.php";
+  ?>
+</div>
