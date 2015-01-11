eea7a6cd17f08e473990a8870c638f19245cedc7
victornicolet <victor.nicolet.0@gmail.com>
2015-01-11 22:21:17 +0100

    Show operation

---
+++ htdocs/view/binet/operation/show.php
@@ -0,0 +1,30 @@
+<link href="../css/user-home.css" rel="stylesheet">
+<div class="show-container">
+  <div class="sh-op-title">
+    <div class="logo">
+      <i class="fa fa-calculator fa-5x"></i>
+    </div>
+    <div class="text">
+      <div class="binet-name">
+        <!-- Nom du binet -->
+      </div>
+      <div class="binet-year">
+        Mandat <!-- Année de mandat du binet-->
+      </div>
+    </div>
+  </div>
+  <div class="sh-op-amount">
+    <!--amount--> <i class="fa fa-euro"></i>
+  </div>
+  <div class="sh-op-refs">
+    <i class="fa fa-fw fa-folder-o"></i> </br>
+    <!--type et numéro de facture ou "Aucune facture associée"--> </br>
+    <!--type et numéro de paiement ou "Aucune paiement associée"-->
+  </div>
+  <div class="sh-op-info">
+    <!-- description de l'opération-->
+  </div>
+  <div class="sh-op-payer">
+    <i class="fa fa-fw fa-user"></i> <!-- Nom de celui qui a payé -->
+  </div>
+</div>
