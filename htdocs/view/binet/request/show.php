b0bd9793ac8526a59588648df82337e76db5920c
victornicolet <victor.nicolet.0@gmail.com>
2015-01-12 22:13:53 +0100

    Show request

--- htdocs/view/binet/request/show.php
+++ htdocs/view/binet/request/show.php
@@ -1,1 +1,33 @@
-
+<link href="<?php echo ASSET_PATH; ?>/css/show.css" rel="stylesheet">
+<link href="<?php echo ASSET_PATH; ?>/css/show-request.css" rel="stylesheet">
+<div class="show-container">
+  <div class="sh-req-title">
+    <div class="logo">
+      <i class="fa fa-5x fa-money"></i>
+    </div>
+    <div class="text">
+      <p class="binet-name">
+        <!-- TODO nom du binet-->
+      </p>
+      <p class="wave-name">
+        <!-- TODO nom conventionnel de la vague-->
+      </p>
+    </div>
+  </div>
+  <!-- TODO itérer pour chaque budget-->
+  <!-- DEBUT -->
+  <div class="sh-req-budget" onclick=""> <!-- TODO : show le budget en question -->
+    <div class="header">
+      <span class="name"><!-- TODO nom du budget--></span>
+    </div>
+    <div class="content">
+      <p class="amount">
+        <!--TODO montant de la demande--> <i class="fa fa-fw fa-euro"></i>
+      </p>
+      <p class="text">
+        <!-- TODO justification de la demande -->
+      </p>
+    </div>
+  </div>
+  <!-- FIN -->
+</div>
