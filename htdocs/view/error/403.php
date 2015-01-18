a5be8199708feaa58dd02d3d73aa3307af3b6f08
victornicolet <victor.nicolet.0@gmail.com>
2015-01-18 18:40:01 +0100

    Error page : error in a panel with a background

--- htdocs/view/error/403.php
+++ htdocs/view/error/403.php
@@ -1,14 +1,9 @@
-<div id="error" class="container">
-  <div class="row">
-    <div class="col-lg-2">
-    </div>
-    <div class="col-lg-8">
-      <div class="jumbotron">
-        <h1><i class="fa fa-fw fa-minus-circle"></i> 403 error</h1>
-        <p>Forbidden</p>
-      </div>
-    </div>
-    <div class="col-lg-2">
+<div id="error-wrapper">
+  <div id="centering">
+    <div class="error-container">
+      <p id="art"><i class="fa fa-fw fa-minus-circle"></i></p>
+      <p id="errorcode">403</p>
+      <p id="message">Forbidden</p>
     </div>
   </div>
 </div>
--- htdocs/view/error/404.php
+++ htdocs/view/error/404.php
@@ -1,14 +1,9 @@
-<div id="error" class="container">
-  <div class="row">
-    <div class="col-lg-2">
-    </div>
-    <div class="col-lg-8">
-      <div class="jumbotron">
-        <h1><i class="fa fa-fw fa-ambulance"></i> 404 error</h1>
-        <p>Not found</p>
-      </div>
-    </div>
-    <div class="col-lg-2">
+<div id="error-wrapper">
+  <div id="centering">
+    <div class="error-container">
+      <p id="art"><i class="fa fa-fw fa-ambulance"></i></p>
+      <p id="errorcode">404</p>
+      <p id="message">Not found</p>
     </div>
   </div>
 </div>
