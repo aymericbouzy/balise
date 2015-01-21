2dfdf78287a166ab0c156ab1106d015e33b35a12
victornicolet <victor.nicolet.0@gmail.com>
2015-01-21 17:39:51 +0100

    Nicer flash

--- htdocs/view/layout/flash.php
+++ htdocs/view/layout/flash.php
@@ -2,23 +2,14 @@
 
   function print_flash($class) {
     if (!empty($_SESSION[$class])) {
-      $cssClass= "alert-default";
-      if($class== "notice"){
-        $cssClass= "alert-success";
-      }else if($class== "error"){
-        $cssClass= "alert-danger":
-      }
       foreach ($_SESSION[$class] as $flash) {
         ?>
-        <div class="col-lg-4">
-          <div class="alert <?php echo $cssClass ?> alert-dismissible fade in" role="alert">
-            <button type="button" class="close" data-dismiss="alert">
-              <span aria-hidden="true">×</span><span class="sr-only">Fermer</span></button>
-              <strong>
-                <?php echo $flash; ?>
-              </strong>
+          <div class="flashcard <?php echo $flash ?> alert alert-dismissible fade in">
+            <button class="close" data-dismiss="alert">
+              <i class="fa fa-fw fa-close"></i>
+            </button>
+            <?php echo $flash; ?>
           </div>
-        </div>
         <?php
       }
       unset($_SESSION[$class]);
@@ -26,7 +17,7 @@
   }
 ?>
 
-  <div class="row">
+  <div class="flash-container">
     <?php
       print_flash("notice");
       print_flash("error");
