b7b02589886720cde4d997f754fc2a7c63ce1fd9
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-01-24 16:41:14 +0100

    finished moving new files to beta

--- htdocs/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include "global/initialisation.php";
-
-  $test = send_email(51, "Nouvelle vague de subventions", "new_wave", array("wave" => 1, "binet" => 1, "term" => 2012));
-
-  var_dump($test);
