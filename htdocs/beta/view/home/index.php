5e22c0cf8a35f74ef6201320c5b5b6a4eb2ea29f
victornicolet <victor.nicolet.0@gmail.com>
2015-01-24 15:58:11 +0100

    Add home index page and base css

---
+++ htdocs/beta/view/home/index.php
@@ -0,0 +1,24 @@
+<div class="home-wrapper">
+  <!-- TODO : si new, les mettre dans le flashcard news -->
+  <div class="flashcard opanel news alert alert-dismissible fade in">
+    <button class="close" data-dismiss="alert">
+      <i class="fa fa-fw fa-close"></i>
+    </button>
+    <h1><!-- TODO : mettre le titre de l'info ici --></h1>
+    <p>
+      <!-- TODO mettre le message de l'info ici -->
+    </p>
+    <div class="controls">
+      <!-- TODO : ici des exemples de contrôles -- WIP : encore maldéfini, et peut $etre pas nécéssaire -->
+      <span class="ctrl"><i class="fa fa-fw fa-cog bt"></i><span class="which-action">Paramètres</span></span>
+      <span class="ctrl"><i class="fa fa-fw fa-check bt"></i><span class="which-action">Valider</span></span>
+    </div>
+  </div>
+  <!--TODO Pour chaque binet un 'spot' -->
+  <!-- TODO si pas de binet afficher un message sympa : vous n'avez pas de binet -->
+  <div class="spot opanel">
+    <!-- TODO : metre l'image du binet : elle doit être carrée -->
+    <!-- TODO Victor : créer une image par défaut, taille 100*100px -->
+    <img src="imagedubinet-carrée">
+  </div>
+</div>
