667630fb57c887d69833c8c35404c3944227293d
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-01-25 23:18:56 +0100

    sanity corrections again

--- htdocs/beta/view/layout/sidebar.php
+++ htdocs/beta/view/layout/sidebar.php
@@ -18,41 +18,41 @@
     </li>
     <!-- Accueil : links to budget/operations page -->
     <?php
-    echo li_link(
-      link_to(path("", "budget", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-home\"></i> Accueil"),
-      $_GET["controller"] == "budget" || $_GET["controller"] == "operation"
-    );
-    $number_pending_validations = count_pending_validations($binet, $term);
-    echo li_link(
-      link_to(
-        path("", "validation", "", binet_prefix($binet, $term)),
-        "<i class=\"fa fa-fw fa-check\"></i> Validations".($number_pending_validations > 0 ? " <span class=\"counter\">".$number_pending_validations."</span>" : "")
-      ),
-      $_GET["controller"] == "validation"
-    );
-    echo li_link(
-      link_to(path("", "request", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Subventions"),
-      $_GET["controller"] == "request"
-    );
-    // If subsidy provider
-    if (select_binet($binet, array("subsidy_provider"))["subsidy_provider"] == 1) {
-      ?>
-        <li class="divider"></li>
-      <?php
       echo li_link(
-      link_to(path("", "wave", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-star\"></i> Vague de subventions"),
-        $_GET["controller"] == "wave"
+        link_to(path("", "budget", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-home\"></i> Accueil"),
+        $_GET["controller"] == "budget" || $_GET["controller"] == "operation"
       );
-    }
-    if ($binet == KES_ID) {
-      ?>
-        <li class="divider"></li>
-      <?php
+      $number_pending_validations = count_pending_validations($binet, $term);
       echo li_link(
-        link_to(path("admin", "binet"), "<i class=\"fa fa-fw fa-desktop\"></i> Administration"),
-        $_GET["controller"] == "admin"
+        link_to(
+          path("", "validation", "", binet_prefix($binet, $term)),
+          "<i class=\"fa fa-fw fa-check\"></i> Validations".($number_pending_validations > 0 ? " <span class=\"counter\">".$number_pending_validations."</span>" : "")
+        ),
+        $_GET["controller"] == "validation"
       );
-    }
-  ?>
-</ul>
+      echo li_link(
+        link_to(path("", "request", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-money\"></i> Subventions"),
+        $_GET["controller"] == "request"
+      );
+      // If subsidy provider
+      if (select_binet($binet, array("subsidy_provider"))["subsidy_provider"] == 1) {
+        ?>
+        <li class="divider"></li>
+        <?php
+        echo li_link(
+        link_to(path("", "wave", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-star\"></i> Vague de subventions"),
+          $_GET["controller"] == "wave"
+        );
+      }
+      if ($binet == KES_ID) {
+        ?>
+        <li class="divider"></li>
+        <?php
+        echo li_link(
+          link_to(path("admin", "binet"), "<i class=\"fa fa-fw fa-desktop\"></i> Administration"),
+          $_GET["controller"] == "admin"
+        );
+      }
+    ?>
+  </ul>
 </div>
