d1611e12918447673cdbb95e8c3c2a0c382b9935
victornicolet <victor.nicolet.0@gmail.com>
2015-01-25 22:17:24 +0100

    Previous commit erased - need a text only pretty-printer

--- htdocs/beta/view/layout/sidebar.php
+++ htdocs/beta/view/layout/sidebar.php
@@ -3,13 +3,13 @@
     <!-- Choose binet using dropdown menu -->
     <li>
       <!-- For all binets -->
-    	<a href="javascript:;" data-target="#binets" data-toggle="collapse">#nomDuBinet</a>
+    	<a href="javascript:;" data-target="#binets" data-toggle="collapse"><?php echo pretty_binet_nolink($binet)?> </a>
 				<ul id="binets" class="collapse">
 						<?php foreach(binet_admins_current_student() as $binet_admin) {
 							$binet_admin["binet_name"] = select_binet($binet_admin["binet"], array("name"))["name"];
 							?>
 							<li>
-								<?php echo link_to(path("", "binet", binet_term_id($binet_admin["binet"], $binet_admin["term"])), $binet_admin["binet_name"]."<span class=\"binet-term\">".$binet_admin["term"]."</span>"); ?>
+								<?php echo link_to(path("", "binet", binet_term_id($binet_admin["binet"], $binet_admin["term"])), $binet_admin["binet_name"]." <span class=\"binet-term\">".$binet_admin["term"]."</span>"); ?>
 							</li>
 							<?php
 							}
