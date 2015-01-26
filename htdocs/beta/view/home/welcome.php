e8cb96ee6addfea6bd75a329505db930ad783238
victornicolet <victor.nicolet.0@gmail.com>
2015-01-26 10:49:29 +0100

    Coquille

--- htdocs/beta/view/home/welcome.php
+++ htdocs/beta/view/home/welcome.php
@@ -7,7 +7,7 @@
 
  <div id="presentation" class="opanel">
    <p>
-     Bienvenu sur la version beta du projet Balise ! Les informations sur le site sont pour l'instant <b>fictives</b>. Nous t'invitons à naviguer sur le site, à tester les fonctionnalités et à nous faire remonter tous les bugs rencontrés grâce au bouton en bas à droite.
+     Bienvenue sur la version beta du projet Balise ! Les informations sur le site sont pour l'instant <b>fictives</b>. Nous t'invitons à naviguer sur le site, à tester les fonctionnalités et à nous faire remonter tous les bugs rencontrés grâce au bouton en bas à droite.
    </p>
    <p>
      Lorsque tu rencontres un bug, essaye de décrire aussi précisément que possible les étapes qui l'ont généré. Pense également à inclure une capture d'écran s'il s'agit d'un bug d'affichage. La personne que nous jugerons la plus utile pour le debbugage du site recevra une récompense ! Tu peux aussi l'utiliser pour nous faire tes remarques, suggestions et compliments ;)
