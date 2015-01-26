532c12cce23194eb1e84db72d5a0ab492d7d089b
victornicolet <victor.nicolet.0@gmail.com>
2015-01-26 12:56:25 +0100

    Default binet image

--- htdocs/beta/view/home/index.php
+++ htdocs/beta/view/home/index.php
@@ -17,8 +17,19 @@
   <!--TODO Pour chaque binet un 'spot' -->
   <!-- TODO si pas de binet afficher un message sympa : vous n'avez pas de binet -->
   <div class="spot opanel">
-    <!-- TODO : metre l'image du binet : elle doit être carrée -->
-    <!-- TODO Victor : créer une image par défaut, taille 100*100px -->
-    <img src="imagedubinet-carrée">
+    <?php
+      $binet_has_image=false;
+      if($binet_has_image){
+        ?>
+        <!-- TODO : metre l'image du binet : elle doit être carrée -->
+        <img src="imagedubinet-carrée">
+    <?php
+    }
+    else {
+    ?>
+      <img src="<?php echo ASSET_PATH; ?>img/binet.png">
+      <?php
+    }
+    ?>
   </div>
 </div>
