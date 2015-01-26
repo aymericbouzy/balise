c7d4d659ee44e4217ca84e430fe41de1355a6f6e
victornicolet <victor.nicolet.0@gmail.com>
2015-01-26 10:36:54 +0100

    Login page v2

--- htdocs/beta/view/home/welcome.php
+++ htdocs/beta/view/home/welcome.php
@@ -1,31 +1,13 @@
- <!-- Very small CSS for login page : if needed, move to another file -->
-<style>
-  	body {
-     	margin-top:0px;
-    	margin-left:-225px;
-    	height: 225px;
-    	background-color: #eee;
-   		}
- 	.signin {
-   	margin-top: auto;
-  		max-width: 330px;
-  		padding: 15px;
-   	margin: 0 auto;
-   	}
-</style>
-
-
   <!-- page-wrapper content : simple button to login using Frankiz-->
   <!-- TODO : update to show a nicer homepage -->
- <div class="container">
-    <div class="signin">
-       <?php echo link_to(path("login", "home"), "<h3>Connexion via Frankiz</h3>", array("class" => "btn btn-primary")); ?>
-    </div>
+<div id="welcome">
+ <div id="login"class="opanel" onclick="goto('login')">
+       Connexion via Frankiz
  </div>
 
- <div class="well well-lg">
+ <div id="presentation" class="opanel">
    <p>
-     Bienvenu sur la version beta du projet Balise ! Les informations sur le site sont pour l'instant <b>fictives</b>. Nous t'invitons à naviguer sur le site, à tester les fonctionnalités et à nous faire remonter tous les bugs rencontrés grâce au bouton en bas à droite.
+     Bienvenue sur la version beta du projet Balise ! Les informations sur le site sont pour l'instant <b>fictives</b>. Nous t'invitons à naviguer sur le site, à tester les fonctionnalités et à nous faire remonter tous les bugs rencontrés grâce au bouton en bas à droite.
    </p>
    <p>
      Lorsque tu rencontres un bug, essaye de décrire aussi précisément que possible les étapes qui l'ont généré. Pense également à inclure une capture d'écran s'il s'agit d'un bug d'affichage. La personne que nous jugerons la plus utile pour le debbugage du site recevra une récompense ! Tu peux aussi l'utiliser pour nous faire tes remarques, suggestions et compliments ;)
@@ -36,5 +18,6 @@
    <p>
      L'équipe du projet Balise.
    </p>
+  </div>
  </div>
  <!-- /container -->
