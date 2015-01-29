bfb8586b84ac15a367fc8349d8dfb092828f33bc
victornicolet <victor.nicolet.0@gmail.com>
2015-01-29 12:33:12 +0100

    Fuzzy search + css upgrade

--- htdocs/beta/view/binet/index.php
+++ htdocs/beta/view/binet/index.php
@@ -2,49 +2,62 @@
   <div id="action-header" class="opanel2">
     <div id="action-title">Binets</div>
     <div class="searchbar">
-      <form class="form">
-        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
-        <input type="search" class="form-control pull-left">
-      </form>
+      <input type="search" class="fuzzy-search"><i class="fa fa-fw fa-search"></i>
     </div>
     <div class="alpha-selecter">
       <!-- Ici utilisation d'ancres seulement : on peut garder des href="#ancre"-->
+      <!-- TODO : voir comment implémenter ca -->
       <a href="#">A</a>
       <a href="#">B</a>
       <a href="#">C</a>
       <a href="#">D</a>
     </div>
   </div>
-  <div class="content-line-panel">
-    <?php
-    /* si kes besoin d'alerter sur l'état du binet */
-    $binet_state_color="green";
-    ob_start();
-    echo "<div><i class=\"fa fa-3x fa-group\"></i>
-    <span class=\"name\">Nom du binet</span>";
-    /* TODO : état du binet si KES*/
-    echo "<span class=\"state ".$binet_state_color."-background\">Etat du binet</span>";
-    /* TODO : couleur d'arrière plan des dates */
-    /* On fait confiance à l'utilisateur pour reconnaitre date limite depot demande
-    et limite utilisation ? */
-    echo "<span class=\"users\">
-    <span class=\"prez\">Jacques Lacan</span>
-    <span class=\"trez\">Johhnie Walker</span>
-    </span>
-    <span class=\"amount ".$binet_state_color."-background\"> 200.00 </span>
-    </div>";
-
-    $content = ob_get_clean();
-
-    echo link_to(path('',''), $content, array("class"=>"opanel clickable-main","goto"=>true));
-
-    /* Here we put one "immediate" action depending on the user */
-    ?>
-    <span class="actions">
+  <ul class="list">
+    <?php for( $i=1 ; $i<=10 ;$i++){?>
+    <li class="content-line-panel">
       <?php
-      echo button(path("",""), "Modifier", "edit", "grey");
-      ?>
+      /* si kes besoin d'alerter sur l'état du binet */
+      $binet_state_color="green";
+      ob_start();
+      echo "<div><i class=\"fa fa-3x fa-group\"></i>
+      <span class=\"name\">Nom du binet ".$i." </span>";
+      /* TODO : état du binet si KES*/
+      echo "<span class=\"state ".$binet_state_color."-background\">Etat du binet</span>";
+      /* TODO : couleur d'arrière plan des dates */
+      /* On fait confiance à l'utilisateur pour reconnaitre date limite depot demande
+      et limite utilisation ? */
+      echo "<span class=\"users\">
+      <span class=\"prez\">Jacques Lacan</span>
+      <span class=\"trez\">Johhnie Walker</span>
+      </span>
+      <span class=\"amount ".$binet_state_color."-background\"> 200.00 </span>
+      </div>";
 
-    </span>
-  </div>
+      $content = ob_get_clean();
+
+      echo link_to(path('',''), $content, array("class"=>"opanel clickable-main","goto"=>true));
+
+      /* Here we put one "immediate" action depending on the user */
+      ?>
+      <span class="actions">
+        <?php
+        echo button(path("",""), "Contacter", "paper-plane", "grey");
+        ?>
+      </span>
+    </li>
+    <?php } ?>
+  </ul>
 </div>
+<!-- Tout est ici pour le filtre sur les binets
+Un helper avec les paramètres "container_name": ici 'public-index-wrapper'
+                              "attribute_name":ici 'name'
+serait utile ! -->
+<script src="<?php echo ASSET_PATH; ?>js/filter.js"></script>
+<script src="<?php echo ASSET_PATH; ?>js/list.fuzzysearch.js"></script>
+<script>
+var objects_list = new List('public-index-wrapper', {
+  valueNames: ['name'],
+  plugins: [ ListFuzzySearch() ]
+});
+</script>
--- htdocs/beta/view/wave/index.php
+++ htdocs/beta/view/wave/index.php
@@ -2,10 +2,8 @@
   <div id="action-header" class="opanel2">
     <div id="action-title">Vagues de subventions</div>
     <div class="searchbar">
-      <form class="form">
-        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
-        <input type="search" class="form-control pull-left">
-      </form>
+        <input type="search" class="fuzzy-search">
+        <i class="fa fa-fw fa-search"></i>
     </div>
     <div class="alpha-selecter">
       <a href="#">2015</a>
@@ -14,32 +12,44 @@
       <a href="#">2012</a>
     </div>
   </div>
-  <div class="content-line-panel">
-    <?php ob_start();
-      echo "<div><i class=\"fa fa-3x fa-money\"></i>
-      <span class=\"name\">Nom de la subvention</span>";
-      /* TODO : couleur d'arrière plan de la subvention */
-      echo "<span class=\"state orange-background\">Etat de la subvention</span>";
-      /* TODO : couleur d'arrière plan des dates */
-      /* On fait confiance à l'utilisateur pour reconnaitre date limite depot demande
-      et limite utilisation ? */
-      echo "<span class=\"dates\">
-        <span class=\"top green-background\">21/07/2015</span>
-        <span class=\"bottom orange-background\">05/09/2015</span>
-      </span>
-      <span class=\"amount green-background\"> 200.00 </span>
-      </div>";
+  <ul class="list">
+    <?php for( $i=1 ; $i<=10 ;$i++){?>
+    <li class="content-line-panel">
+      <?php ob_start();
+        echo "<div><i class=\"fa fa-3x fa-money\"></i>
+        <span class=\"name\">Nom de la subvention</span>";
+        /* TODO : couleur d'arriï¿½re plan de la subvention */
+        echo "<span class=\"state orange-background\">Etat de la subvention</span>";
+        /* TODO : couleur d'arriï¿½re plan des dates */
+        /* On fait confiance ï¿½ l'utilisateur pour reconnaitre date limite depot demande
+        et limite utilisation ? */
+        echo "<span class=\"dates\">
+          <span class=\"top green-background\">21/07/2015</span>
+          <span class=\"bottom orange-background\">05/09/2015</span>
+        </span>
+        <span class=\"amount green-background\"> 200.00 </span>
+        </div>";
 
-      $content = ob_get_clean();
+        $content = ob_get_clean();
 
-      echo link_to(path('',''), $content, array("class"=>"opanel clickable-main","goto"=>true));
+        echo link_to(path('',''), $content, array("class"=>"opanel clickable-main","goto"=>true));
 
-      /* Here we put one "immediate" action depending on the user */
-      ?>
-      <span class="actions">
-        <?php
-        echo button(path("",""), "Modifier", "edit", "grey");
-      ?>
-      </span>
-    </div>
+        /* Here we put one "immediate" action depending on the user */
+        ?>
+        <span class="actions">
+          <?php
+          echo button(path("",""), "Demander des subventions", "question", "green");
+        ?>
+        </span>
+      </li>
+      <?php } ?>
+  </ul>
 </div>
+<script src="<?php echo ASSET_PATH; ?>js/filter.js"></script>
+<script src="<?php echo ASSET_PATH; ?>js/list.fuzzysearch.js"></script>
+<script>
+  var objects_list = new List('public-index-wrapper', {
+    valueNames: ['name'],
+    plugins: [ ListFuzzySearch() ]
+  });
+</script>
