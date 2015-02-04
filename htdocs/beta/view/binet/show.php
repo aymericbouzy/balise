51740d9908994c6512bbbc911acda811ce2f97a7
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-04 18:01:47 +0100

    sanitize - removes budget details

--- htdocs/beta/view/binet/show.php
+++ htdocs/beta/view/binet/show.php
@@ -58,120 +58,82 @@
       Administrateurs
     </span>
     <?php
-    $admins = select_current_admins($binet["id"]);
-    if(!empty($admins))
-    {
-      foreach ($admins as $admin) {
+      $admins = select_current_admins($binet["id"]);
+      if (!empty($admins)) {
+        foreach ($admins as $admin) {
+          ?>
+          <span class="admin">
+            <i class="fa fa-fw fa-user logo"></i>
+            <i class="fa fa-fw fa-send logo"></i>
+            <?php echo pretty_student($admin["id"]); ?>
+          </span>
+          <?php
+        }
+      } else {
         ?>
-        <span class="admin">
-          <i class="fa fa-fw fa-user logo"></i>
-          <i class="fa fa-fw fa-send logo"></i>
-          <?php echo pretty_student($admin["id"]); ?>
-        </span>
+        <i style="padding:5px;color:#DD2C00;" class="fa fa-fw fa-warning"></i> Il n'y a aucun administrateur pour ce binet !
         <?php
       }
-    } else {?>
-       <i style="padding:5px;color:#DD2C00;" class="fa fa-fw fa-warning"></i> Il n'y a aucun administrateur pour ce binet !
-    <?php
+      if (is_current_kessier()) {
+        ?>
+        <div class="add">
+          <?php echo button_small(path("new", "admin", "", binet_prefix($binet["id"], $binet["current_term"])), "Ajouter un administrateur", "plus", "green", true); ?>
+          <?php echo button_small(path("index", "admin", "", binet_prefix($binet["id"], $binet["current_term"])), "Supprimer un administrateur", "minus", "red", true); ?>
+        </div>
+        <?php
       }
-    if(is_current_kessier()){ ?>
-      <div class="add">
-        <?php echo button_small(path("new", "admin", "", binet_prefix($binet["id"], $binet["current_term"])),"Ajouter un administrateur","plus","green",true);?>
-        <?php echo button_small("","Supprimer un administrateur","minus","red",true);?>
-      </div>
-    <?php } ?>
+    ?>
   </div>
   <div class="sh-block-normal opanel">
-    <?php echo $binet["description"];?>
-  </div>
-  <?php if(has_viewing_rights($binet["id"],$binet["current_term"])) {
-    $real_income = 0;
-    $income = 0;
-    $real_spending = 0;
-    $spending = 0;
-    foreach($budgets as $budget){
-      if($budget["amount"]>0){
-        $real_income+=$budget["real_amount"];
-        $income+=$budget["amount"];
-      }
-      else {
-        $real_spending+=$budget["real_amount"];
-        $spending+=$budget["amount"];
-      }
-    }
-      ?>
-  <div class="sh-bin-stats light-blue-background opanel">
-    <div class="minipane" id="income">
-      <div class="title">Recettes</div><?php echo pretty_amount($real_income);?>/
-      <span><?php echo pretty_amount($income);?></span>
-      </div>
-    <div class="minipane" id="spending">
-      <div class="title">Dépenses</div><?php echo pretty_amount($real_spending);?>/
-      <span><?php echo pretty_amount($spending);?></span>
-      </div>
-    <div class="minipane" id="balance">
-      <div class="title">Equilibre</div>
-      <?php echo pretty_amount(sum_array($budgets,"real_amount")); ?> /
-      <span><?php echo pretty_amount(sum_array($budgets,"amount")); ?></span>
-    </div>
-    <div class="minipane" id="subsidies_granted">
-      <div class="title">Subventions accordées</div>
-      <?php echo pretty_amount(sum_array($budgets,"subsidized_amount_granted")); ?>
-      </div>
-    <div class="minipane" id="subsidies_used">
-      <div class="title">Subventions utilisées</div>
-    <?php echo pretty_amount(sum_array($budgets,"subsidized_amount_used")); ?>
-    </div>
-  </div>
-  <div class="sh-bin-resume light-blue-background opanel">
-    <div class="title">
-      Dépenses
-    </div>
-    <?php foreach($budgets as $budget){
-      if($budget["amount"]<0){
-      ?>
-    <div class="line">
-      <span class="label"><?php echo pretty_budget($budget["id"]);?></span>
-      <span class="amount"><?php echo pretty_amount($budget["real_amount"]);?></span>
-    </div>
-    <?php
-      }
-    } ?>
-  </div>
-  <div class="sh-bin-resume light-blue-background opanel">
-    <div class="title">
-      Recettes
-    </div>
-    <?php foreach($budgets as $budget){
-      if($budget["amount"]>0){
-        ?>
-        <div class="line">
-          <span class="label"><?php echo pretty_budget($budget["id"]);?></span>
-          <span class="amount"><?php echo pretty_amount($budget["real_amount"]);?></span>
-        </div>
-        <?php
-      }
-    } ?>
+    <?php echo $binet["description"]; ?>
   </div>
   <?php
-  }
-  if(!empty($waves)) {
-    ?>
-  <div class="sh-bin-resume light-blue-background opanel">
-    <div class="title">
-      Vagues de subventions
-    </div>
-    <?php foreach($waves as $wave){
-        ?>
-        <div class="line">
-          <span class="label"><?php echo pretty_wave($wave["id"]);?></span>
-          <span class="submission date"><?php echo $wave["submission_date"];?></span>
-            <span class="expiry date"> <?php echo $wave["expiry_date"];?></span>
+    if (has_viewing_rights($binet["id"], $binet["current_term"])) {
+      ?>
+      <div class="sh-bin-stats light-blue-background opanel">
+        <div class="minipane" id="income">
+          <div class="title">Recettes</div><?php echo pretty_amount($real_income);?>/
+          <span><?php echo pretty_amount($income);?></span>
+          </div>
+        <div class="minipane" id="spending">
+          <div class="title">Dépenses</div><?php echo pretty_amount($real_spending);?>/
+          <span><?php echo pretty_amount($spending);?></span>
+          </div>
+        <div class="minipane" id="balance">
+          <div class="title">Equilibre</div>
+          <?php echo pretty_amount(sum_array($budgets,"real_amount")); ?> /
+          <span><?php echo pretty_amount(sum_array($budgets,"amount")); ?></span>
+        </div>
+        <div class="minipane" id="subsidies_granted">
+          <div class="title">Subventions accordées</div>
+          <?php echo pretty_amount(sum_array($budgets,"subsidized_amount_granted")); ?>
+          </div>
+        <div class="minipane" id="subsidies_used">
+          <div class="title">Subventions utilisées</div>
+        <?php echo pretty_amount(sum_array($budgets,"subsidized_amount_used")); ?>
+        </div>
+      </div>
+      <?php
+    }
+    if (!empty($waves)) {
+      ?>
+      <div class="sh-bin-resume light-blue-background opanel">
+        <div class="title">
+          Vagues de subventions
         </div>
         <?php
-    } ?>
-  </div>
-  <?php
+          foreach($waves as $wave) {
+            ?>
+            <div class="line">
+              <span class="label"><?php echo pretty_wave($wave["id"]); ?></span>
+              <span class="submission date"><?php echo $wave["submission_date"]; ?></span>
+                <span class="expiry date"> <?php echo $wave["expiry_date"]; ?></span>
+            </div>
+            <?php
+          }
+        ?>
+      </div>
+      <?php
     }
-    ?>
+  ?>
 </div>
