9a0c980b869cba1148b796e58b78304488f1352f
victornicolet <victor.nicolet.0@gmail.com>
2015-02-06 10:50:44 +0100

    Button added : go back to show binet

--- htdocs/beta/view/binet/admin/index.php
+++ htdocs/beta/view/binet/admin/index.php
@@ -1,4 +1,7 @@
 <div id="admin-wrapper">
+<div class="actionbar-left">
+  <?php echo button(path("show", "binet", $binet),"","chevron-left","blue",true,"");?>
+</div>
 <?php
 $admins = select_current_admins($binet);
 if (!empty($admins)) {
@@ -8,7 +11,7 @@ if (!empty($admins)) {
       <i class="fa fa-fw fa-user logo"></i>
       <i class="fa fa-fw fa-send logo"></i>
       <?php echo pretty_student($admin["id"]);
-      /* TODO : path */
+
         echo button(path("delete", "admin", $admin["id"], binet_prefix($binet, $term), array(), true),"Retirer cet administrateur","times","red",true,"small");?>
     </span>
     <?php
