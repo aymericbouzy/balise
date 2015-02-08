2de379b3f77d29e267b44fbda89f4095cff0a8ce
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-08 21:34:35 +0100

    feature implemented

--- htdocs/beta/view/layout/footer.php
+++ htdocs/beta/view/layout/footer.php
@@ -18,12 +18,13 @@
 
   $reference = substr(md5(rand()), 0, 10);
   $url = $_SERVER["REDIRECT_URL"];
+	$browser = $_SERVER["HTTP_USER_AGENT"];
   $email = connected_student() ? select_student($_SESSION["student"], array("email"))["email"] : "";
   $post = array_to_string($_POST);
   $session = array_to_string($_SESSION);
   $get = array_to_string($_GET);
 
-  $body = "\n\n\n\n———————————\n**** Ne pas modifier cette partie ****\n\nURL demandée :\t\t\t\t\t".$url."\npersonne connectée :\t\t\t\t".$email."\nétat de la variable \$_POST :\t\t\t".$post."\nétat de la variable \$_SESSION :\t\t".$session."\nétat de la variable \$_GET :\t\t\t".$get;
+  $body = "\n\n\n\n———————————\n**** Ne pas modifier cette partie ****\n\nURL demandée :\t\t\t\t\t".$url."\nBrowser :\t\t\t\t\t\t\t".$browser."\npersonne connectée :\t\t\t\t".$email."\nétat de la variable \$_POST :\t\t\t".$post."\nétat de la variable \$_SESSION :\t\t".$session."\nétat de la variable \$_GET :\t\t\t".$get;
   $body = urlencode($body);
   $body = str_replace(array("+"), array(" "), $body);
 
