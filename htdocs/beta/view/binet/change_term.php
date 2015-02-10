d348113579214d528149a976f75c3052560d40d9
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-10 15:21:38 +0100

    updated view with new paths

--- htdocs/beta/view/binet/change_term.php
+++ htdocs/beta/view/binet/change_term.php
@@ -3,8 +3,9 @@
   if (is_empty($binet["term"])) {
     ?>
     <h1>Réactiver le binet</h1>
-    <form role="form" id="change_term_binet" action="/<?php echo path("set_term", "binet", $binet["id"], "", array(), true); ?>" method="post">
+    <form role="form" id="change_term_binet" action="/<?php echo path("reactivate", "binet", $binet["id"], ""); ?>" method="post">
       <?php echo form_group_text("Promotion :", "term", $binet, "binet"); ?>
+      <?php echo form_csrf_token(); ?>
       <?php echo form_submit_button("Réactiver"); ?>
     </form>
     <?php
@@ -14,7 +15,7 @@
     <div class="buttons">
       <?php
         $new_term = $binet["term"] + 1;
-        echo link_to(path("set_term", "binet", $binet["id"], "", array("term" => $new_term), true), "Passer à la promo ".$new_term, array("class" => "btn btn-primary"));
+        echo link_to(path("power_transfer", "binet", $binet["id"], "", array("term" => $new_term), true), "Passer à la promo ".$new_term, array("class" => "btn btn-primary"));
         echo "\t";
         echo link_to(path("deactivate", "binet", $binet["id"], "", array(), true), "Désactiver le binet",  array("class" => "btn btn-danger"));
       ?>
