66aa767a6b006f3ac9bee16a83c808989b5ced58
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-10 10:06:54 +0100

    link for binet admin corrected

--- htdocs/beta/view/layout/sidebar.php
+++ htdocs/beta/view/layout/sidebar.php
@@ -53,7 +53,7 @@
         <li class="divider"></li>
         <?php
         echo li_link(
-          link_to(path("admin", "binet"), "<i class=\"fa fa-fw fa-desktop\"></i> Administration"),
+          link_to(path("", "admin", "", binet_prefix($binet, $term)), "<i class=\"fa fa-fw fa-desktop\"></i> Administration"),
           $_GET["controller"] == "admin"
         );
       }
