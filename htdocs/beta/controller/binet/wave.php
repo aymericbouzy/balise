8a174ca5d7a84df0aa8937e9527f1ef9562ba7a6
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-11 10:14:51 +0100

    corrections

--- htdocs/beta/controller/binet/wave.php
+++ htdocs/beta/controller/binet/wave.php
@@ -66,7 +66,7 @@
     break;
 
   case "show":
-    $wave = select_wave($wave["id"], array("id", "submission_date", "expiry_date", "published", "binet", "term", "state"));
+    $wave = select_wave($wave["id"], array("id", "submission_date", "expiry_date", "published", "binet", "term", "state", "requested_amount", "granted_amount"));
     break;
 
   case "edit":
