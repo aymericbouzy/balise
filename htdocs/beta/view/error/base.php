15010b34933568d38f25bef86642824753353b9b
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-12 13:36:13 +0100

    return to website after error button

--- htdocs/beta/view/error/base.php
+++ htdocs/beta/view/error/base.php
@@ -4,7 +4,10 @@
       <p id="art"><i class="fa fa-fw <?php echo $error_icon; ?>"></i></p>
       <p id="errorcode"><?php echo $_GET["action"]; ?></p>
       <p id="message"><?php echo $error_message; ?></p>
-      <?php // echo link_to($_SERVER["HTTP_REFERER"], "Retourner sur le site", array("class" => "btn btn-primary btn-lg")); ?>
+      <?php
+        $redirect_path = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : path("", "home");
+        echo link_to($redirect_path, "Retourner sur le site", array("class" => "btn btn-primary btn-lg"));
+      ?>
     </div>
   </div>
 </div>
