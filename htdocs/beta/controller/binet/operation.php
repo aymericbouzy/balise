bd806eb75af4dc2bb8462deaf513635d0451775a
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-12 15:21:49 +0100

    corrected sign bug

--- htdocs/beta/controller/binet/operation.php
+++ htdocs/beta/controller/binet/operation.php
@@ -76,6 +76,7 @@
     break;
 
   case "create":
+    set_if_not_set($_POST["sign"], 0);
     $operation["id"] = create_operation($binet, $term, (1 - 2*$_POST["sign"])*$_POST["amount"], $_POST["type"], $_POST);
     $_SESSION["notice"][] = "L'opération a été créée avec succès. Il vous reste à indiquer à quel(s) budget(s) cette opération se rapporte.";
     redirect_to_action("review");
