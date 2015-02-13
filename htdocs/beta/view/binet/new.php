518aa5be95de96be97fee71657bccb49d3cd151e
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-13 10:11:45 +0100

    removed "mandat" - removed unauthorized links

--- htdocs/beta/view/binet/new.php
+++ htdocs/beta/view/binet/new.php
@@ -2,7 +2,7 @@
   <h1>Nouveau binet</h1>
   <form role="form" id="binet" action="/<?php echo path("create", "binet"); ?>" method="post">
     <?php echo form_group_text("Nom :", "name", $binet, "binet"); ?>
-    <?php echo form_group_text("Promotion du mandat courant :", "term", $binet, "binet"); ?>
+    <?php echo form_group_text("Promotion du binet :", "term", $binet, "binet"); ?>
     <?php echo form_csrf_token(); ?>
     <?php echo form_submit_button("Créer"); ?>
   </form>
