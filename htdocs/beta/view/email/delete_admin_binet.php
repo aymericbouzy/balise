2f4a875bff67cb69f086a9ec718d990de3e9acc7
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-02-16 17:23:07 +0100

    send emails for all admin modifications

---
+++ htdocs/beta/view/email/delete_admin_binet.php
@@ -0,0 +1,3 @@
+<p>
+  Les droits d'administration de <?php echo pretty_student($parameters["admin"]); ?> pour le binet <?php echo pretty_binet_term($parameters["binet_term"], true, false); ?> ont été retirés par <?php echo pretty_student($parameters["kessier"]); ?>.
+</p>
