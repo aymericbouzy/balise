0711318f070b445c9f269d9c7872f5b38f4d3af0
victornicolet <victor.nicolet.0@gmail.com>
2015-02-16 10:21:47 +0100

    Rough drafts

--- htdocs/beta/view/binet/request/index.php
+++ htdocs/beta/view/binet/request/index.php
@@ -7,7 +7,7 @@
       <div id="request-count">
         <!-- Requêtes non-envoyées -->
         <span class="<?php echo request_state("rough_draft")["color"];?>-background">
-           <?php echo text_tune_with_amount($requests_count_by_state["rough_draft"],"non-envoyée"); ?>
+           <?php echo text_tune_with_amount($requests_count_by_state["rough_draft"],"brouillon"); ?>
         </span>
         <!-- Requêtes envoyées : en traitement par le binet subventionneur -->
         <span class="<?php echo request_state("sent")["color"];?>-background white-text">
@@ -31,6 +31,59 @@
       </div>
     </div>
   </div>
+
+  <!-- Rough drafts -->
+  <?php if(sizeOf($rough_drafts) > 0) {?>
+  <div class="panel opanel">
+    <div class="title">
+      Brouillons
+    </div>
+  </div>
+  <?php
+    foreach($rough_drafts as $rough_draft){
+      $draft_state = request_state("rough_draft");
+      $subsidies = select_subsidies(array("request" => $rough_draft["id"]));
+      ?>
+      <div class="panel opanel light-blue-background">
+        <?php echo link_to(path("show", "request", $rough_draft["id"], binet_prefix($binet, $term)),
+        "<i class=\"fa fa-fw fa-eye\"></i> Voir la requête",array("class"=>"btn","id"=>"show-request"));
+        echo link_to(path("send", "request", $request["id"], binet_prefix($binet, $term), array(), true),
+        "<i class=\"fa fa-fw fa-send\"></i> Envoyer",array("class"=>"btn-success btn","id"=>"send-request"));?>
+        <div class="title-small">
+          <?php echo pretty_wave($rough_draft["wave"]);?>
+        </div>
+        <div class="content">
+          <div class="state-indicator <?php echo $draft_state["color"]; ?>-background">
+            <i class="fa fa-fw fa-<?php echo $draft_state["icon"]; ?>"></i>
+          </div>
+          <div class="request-infos">
+            <span><?php echo pretty_amount($rough_draft["requested_amount"],false,true);?> demandés</span>
+          </div>
+          <div class="subsidies panel inside">
+            <div class="content light-blue-background">
+              <?php
+                foreach($subsidies as $subsidy){
+                    $subsidy = select_subsidy($subsidy["id"], array("id","budget", "purpose","requested_amount"));
+                    echo "<span class=\"pill\">".
+                    pretty_budget($subsidy["budget"]).
+                    " <i>".pretty_amount($subsidy["requested_amount"])."</i></span>";
+                }
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
+  <?php
+    }
+  }
+  ?>
+
+  <!-- Requests -->
+  <div class="panel opanel">
+    <div class="title">
+      Subventions précédentes
+    </div>
+  </div>
   <?php
     foreach($requests as $request) {
       $request_state = request_state($request["state"]);
@@ -47,9 +100,9 @@
             <i class="fa fa-fw fa-<?php echo $request_state["icon"]; ?>"></i>
           </div>
           <div class="request-infos">
-            <span><?php echo ($request["sent"] == 0 ? "" : pretty_amount($request["granted_amount"])." accordés / ").
-              pretty_amount($request["requested_amount"])." demandés." ?></span>
-            <span><?php echo ($request["state"] == "accepted" ? pretty_amount($request["used_amount"])." utilisés." : ($request["state"] == "rejected" ? "Subventions refusées" : "")); ?></span>
+            <span><?php echo ($request["sent"] == 0 ? "" : pretty_amount($request["granted_amount"],false)." accordés / ").
+              pretty_amount($request["requested_amount"],false)." demandés." ?></span>
+            <span><?php echo ($request["state"] == "accepted" ? pretty_amount($request["used_amount"],false)." utilisés." : ($request["state"] == "rejected" ? "Subventions refusées" : "")); ?></span>
           </div>
         <?php if($request["state"] != "rejected") { ?>
           <div class="subsidies panel inside">
