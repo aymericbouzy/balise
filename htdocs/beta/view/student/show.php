1bc3893e437cbf1fb9da909502aa9857705477aa
victornicolet <victor.nicolet.0@gmail.com>
2015-02-16 18:39:15 +0100

    Created show

--- htdocs/beta/view/student/show.php
+++ htdocs/beta/view/student/show.php
@@ -1,3 +1,24 @@
-<?php echo $student["name"]; ?>
-<br>
-<?php echo link_to("mailto:".$student["name"]." <".$student["email"].">", $student["email"]); ?>
+<div class="sidebar-not-present">
+  <div class="show-container">
+    <div class="sh-title opanel">
+      <div class="logo">
+        <i class="fa fa-5x fa-user"></i>
+      </div>
+      <div class="text">
+        <span class="main">
+          <?php echo $student["name"]; ?>
+        </span>
+        <span class="sub">
+          <?php echo link_to("mailto:".$student["name"]." <".$student["email"].">", $student["email"]); ?>
+        </span>
+      </div>
+    </div>
+    <div class="panel opanel light-blue-background">
+      <div class="title">
+        Binet administrés
+      </div>
+      <div class="content">
+      </div>
+    </div>
+  </div>
+</div>
