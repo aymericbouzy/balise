6f9e3f86f5d9415559f10d47c640fd88cbdc7693
victornicolet <victor.nicolet.0@gmail.com>
2015-02-18 15:42:35 +0100

    Budget form update

--- htdocs/beta/view/binet/budget/form.php
+++ htdocs/beta/view/binet/budget/form.php
@@ -5,9 +5,9 @@
     }
   ?>
   <?php echo form_group_text("Nom :", "label", $budget, "budget"); ?>
-  <?php echo form_group_text("Tags (séparés par des ';') :", "tags_string", $budget, "budget"); ?>
-  <?php echo form_group_text("Montant prévisionnel :", "amount", $budget, "budget"); ?>
-  <?php echo form_group_text("Subventions espérées :", "subsidized_amount", $budget, "budget"); ?>
+  <?php echo form_group_text("Mots clés (séparés par des ';') :", "tags_string", $budget, "budget",array("placeholder"=>"Les mots clés qui vous permettront de retrouver le budget.")); ?>
+  <?php echo form_group_text("Montant prévisionnel :", "amount", $budget, "budget",array("placeholder"=>"0.00")); ?>
+  <?php echo form_group_text("Subventions espérées :", "subsidized_amount", $budget, "budget",array("placeholder"=>"0.00 - Les subventions que vous espérez recevoir pour ce budget.")); ?>
   <?php echo form_csrf_token(); ?>
   <?php echo form_submit_button($submit_label); ?>
 </form>
