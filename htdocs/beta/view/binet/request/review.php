dba9f34773206e993e2d61a8f738e3b90d35914f
victornicolet <victor.nicolet.0@gmail.com>
2015-02-22 19:21:27 +0100

    Updated request review

--- htdocs/beta/view/binet/request/review.php
+++ htdocs/beta/view/binet/request/review.php
@@ -103,28 +103,37 @@
     <?php
     foreach (select_subsidies(array("request" => $request_info["id"])) as $subsidy) {
       $subsidy = select_subsidy($subsidy["id"], array("id", "budget", "requested_amount", "purpose"));
-      $budget = select_budget($subsidy["budget"], array("id", "label", "binet", "term","real_amount","amount","subsidized_amount_granted","subsidized_amount_used"));
+      $budget = select_budget($subsidy["budget"], array("id", "label", "binet", "term","real_amount","amount","subsidized_amount","subsidized_amount_granted","subsidized_amount_used"));
       ?>
       <div class="panel light-blue-background opanel">
         <?php echo link_to(path("show", "budget", $budget["id"], binet_prefix($budget["binet"], $budget["term"])),
                         "<div class=\"title\">".$budget["label"]."<span><i class=\"fa fa-fw fa-eye\"></i>  Voir le budget</span></div>",
                         array("goto"=>true));?>
         <div class="content">
-          <div class="infos">
-            <p class="amount-requested">
-              <span class="minititle">Montant demandé</span>
-              <?php echo pretty_amount($subsidy["requested_amount"],false,true); ?></i>
-            </p>
-            <p class="budget-summary">
-              <span class="minititle">Résumé du budget</span>
-              <span>Prévisionnel : <?php echo pretty_amount($budget["amount"])?></span>
-              <span> Réel : <?php echo pretty_amount($budget["real_amount"])?></span>
-            </p>
-            <p class="budget-summary">
-              <span class="minititle">Subventions</span>
-              <span>Reçues : <?php echo pretty_amount($budget["subsidized_amount_granted"])?></span>
-              <span>Utilisées : <?php echo pretty_amount($budget["subsidized_amount_used"])?></span>
-            </p>
+          <div class="infos table table-responsive">
+            <table>
+              <thead>
+                <tr>
+                  <td class="minititle" >Montant demandé</td>
+                  <td class="minititle" >Résumé du budget</td>
+                  <td class="minititle" >Subventions</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="summary">
+                  <td rowspan="3" class="amount-requested"><?php echo pretty_amount($subsidy["requested_amount"],false,true); ?></td>
+                  <td> Prévisionnel : <?php echo pretty_amount($budget["amount"])?></td>
+                  <td> Attendues : <?php echo pretty_amount($budget["subsidized_amount"])?></td>
+                </tr>
+                <tr class="summary">
+                  <td> Réel : <?php echo pretty_amount($budget["real_amount"])?></td>
+                  <td> Reçues : <?php echo pretty_amount($budget["subsidized_amount_granted"])?></td>
+                </tr>
+                <tr class="summary">
+                  <td>Utilisées : <?php echo pretty_amount($budget["subsidized_amount_used"])?></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <div class="granted-amount">
           <?php echo form_group_text("Montant accordé :", adds_amount_prefix($subsidy), $request, "request"); ?>
