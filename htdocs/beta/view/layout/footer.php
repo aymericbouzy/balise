ad3ab9a51aa2c6795272c3b1641f9e15ceb3efe3
victornicolet <victor.nicolet.0@gmail.com>
2015-02-27 10:55:50 +0100

    Final correction

--- htdocs/beta/view/layout/footer.php
+++ htdocs/beta/view/layout/footer.php
@@ -1,9 +1,11 @@
-<span>
+<span id="link-to-webmasters">
 	Site créé par <?php echo link_to("mailto:Nathan Eckert <nathan.eckert@polytechnique.edu>", "Nathan"); ?>,
 	<?php echo link_to("mailto:Victor Nicolet <victor.nicolet@polytechnique.edu>", "Little"); ?> et
 	<?php echo link_to("mailto:Aymeric Bouzy <aymeric.bouzy@polytechnique.edu>", "Zouby"); ?>.
 </span>
+<span id="bug-report-action">
 <?php
 	echo modal_toggle("bug-report-toggle", "Rapport de bug", "btn btn-primary", "bug-report");
 	echo modal("bug-report", "Rapport de bug", get_html_form("bug_report"));
 ?>
+</span>
