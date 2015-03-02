5b3ec720a851b5fa934d7b8fef0717f71c81f654
victornicolet <victor.nicolet.0@gmail.com>
2015-03-02 15:54:11 +0100

    Add dropdown

--- htdocs/beta/view/binet/index.php
+++ htdocs/beta/view/binet/index.php
@@ -53,15 +53,35 @@
       }
     ?>
   </ul>
-  <div id="deactivated" class="panel shadowed">
-    <div class="title">
-      Binets qui n'ont pas trouvé de repreneur
-    </div>
-  </div>
-  <ul class="list">
+  <?php
+    $html_content ="<div id=\"deactivated\" class=\"panel shadowed\"><div class=\"title\">".
+    "  Binets qui n'ont pas trouvé de repreneur <i class=\"fa fa-chevron-down fa-fw\"></i> </div></div>";
+    echo make_collapse_control($html_content, "deactivatedBinets_list");
+  ?>
+  <ul class="list collapse" id="deactivatedBinets_list">
     <?php
-      foreach(select_binets(array("current_term" => null)) as $binet){
-
+      foreach(select_binets(array("current_term" => NULL)) as $binet){
+         ?>
+          <li class="content-line-panel">
+          <?php
+          ob_start();
+          ?>
+          <i class="fa fa-fw fa-group deactivated"></i>
+          <span class="name"><?php echo pretty_binet($binet["id"], false); ?></span>
+          <span class="users">
+            <?php
+            foreach (select_current_admins($binet["id"]) as $admin) {
+              ?>
+              <span class="prez"><?php echo "<span class=\"pill\">".pretty_student($admin["id"])."</span>"; ?></span>
+              <?php
+            }
+            ?>
+          </span>
+          <?php
+          echo link_to(path("show", "binet", $binet["id"]), "<div>".ob_get_clean()."</div>\n", array("class"=>"shadowed clickable-main","goto"=>true));
+          ?>
+        </li>
+        <?php
       }
     ?>
   </ul>
