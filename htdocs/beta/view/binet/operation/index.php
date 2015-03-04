0f75d6702982e87abddf171db6092073838f1864
victornicolet <victor.nicolet.0@gmail.com>
2015-03-04 13:36:35 +0100

    Operations waitingfor Kes validation in op index

--- htdocs/beta/view/binet/operation/index.php
+++ htdocs/beta/view/binet/operation/index.php
@@ -1,5 +1,4 @@
 <?php
-
   $budget_view = false;
   $table_title = "Dernières opérations";
   ob_start();
@@ -50,13 +49,50 @@
 <tbody>
   <tr class="total">
     <td colspan="3">Total</td>
-    <td><?php echo pretty_amount(sum_array($operations, "amount", "negative")); ?></td>
-    <td><?php echo pretty_amount(sum_array($operations, "amount", "positive")); ?></td>
+    <td><?php echo pretty_amount(sum_array($operations, "amount", "positive"),false); ?></td>
+    <td><?php echo pretty_amount(sum_array($operations, "amount", "negative"),false); ?></td>
   </tr>
   <tr class="total">
     <td colspan="3">Solde</td>
     <td colspan="2"><b><?php echo pretty_amount(sum_array($operations, "amount")); ?></b></td>
   </tr>
+</tbody>
+<thead class="separator">
+  <tr>
+    <td colspan="5"></td>
+  </tr>
+</thead>
+<tbody>
+  <tr class="grey-background">
+    <td colspan="5">
+      Opérations en attente de validation par la Kès
+    </td>
+  </tr>
+  <?php foreach($op_pending_kes_validations as $operation ){
+      ob_start();
+      ?>
+      <td class="element_name">
+        <?php echo $operation["comment"]; ?>
+      </td>
+      <td class="tags">
+        <?php echo pretty_tags(select_tags_operation($operation["id"]), true); ?>
+      </td>
+      <td>
+        <?php echo pretty_date($operation["date"]); ?>
+      </td>
+      <?php if ($operation["amount"] < 0) {
+        ?><td></td><td>
+          <?php echo pretty_amount($operation["amount"],false); ?>
+        </td><?php
+      } else {
+        ?><td>
+          <?php echo pretty_amount($operation["amount"],false); ?>
+        </td><td></td>
+        <?php
+      }
+      echo link_to(path("show", "operation", $operation["id"], binet_prefix($operation["binet"], $operation["term"])),
+      "<tr>".ob_get_clean()."</tr>",array("goto"=>true));
+    } ?>
 </tbody>
 <?php
   $table = ob_get_clean();
