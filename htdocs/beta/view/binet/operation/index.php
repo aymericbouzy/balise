a9f80edf5f99cb9a8008349d6bfedd370f5dbb20
victornicolet <victor.nicolet.0@gmail.com>
2015-03-04 13:52:10 +0100

    DRY

--- htdocs/beta/view/binet/operation/index.php
+++ htdocs/beta/view/binet/operation/index.php
@@ -1,6 +1,23 @@
 <?php
   $budget_view = false;
   $table_title = "Dernières opérations";
+  $sum_incomes = sum_array($operations, "amount", "positive");
+  $sum_spendings = sum_array($operations, "amount", "negative");
+  $balance = sum_array($operations, "amount");
+  $sum_pending_incomes = sum_array($op_pending_kes_validations, "amount", "positive");
+  $sum_pending_spendings = sum_array($op_pending_kes_validations, "amount", "negative");
+  $pendings_balance = sum_array($op_pending_kes_validations, "amount");
+
+  function operation_line($operation){
+    $line = "<td class=\"element_name\">".$operation["comment"]."</td>
+    <td class=\"tags\">".pretty_tags(select_tags_operation($operation["id"]), true)."</td>
+    <td>".pretty_date($operation["date"])."</td>".
+    (($operation["amount"] < 0) ? ("<td></td><td>".pretty_amount($operation["amount"],false)."</td>"):
+    ("<td>".pretty_amount($operation["amount"],false)."</td><td></td>"));
+
+    return link_to(path("show", "operation", $operation["id"], binet_prefix($operation["binet"], $operation["term"])),
+    "<tr>".$line."</tr>",array("goto"=>true));
+  }
   ob_start();
 ?>
 <thead>
@@ -15,29 +32,7 @@
 <tbody class="list">
   <?php
     foreach ($operations as $operation) {
-      ob_start();
-      ?>
-        <td class="element_name">
-          <?php echo $operation["comment"]; ?>
-        </td>
-        <td class="tags">
-          <?php echo pretty_tags(select_tags_operation($operation["id"]), true); ?>
-        </td>
-        <td>
-          <?php echo pretty_date($operation["date"]); ?>
-        </td>
-        <?php if ($operation["amount"] < 0) {
-          ?><td></td><td>
-            <?php echo pretty_amount($operation["amount"],false); ?>
-          </td><?php
-        } else {
-          ?><td>
-            <?php echo pretty_amount($operation["amount"],false); ?>
-          </td><td></td>
-      <?php
-        }
-      echo link_to(path("show", "operation", $operation["id"], binet_prefix($operation["binet"], $operation["term"])),
-      "<tr>".ob_get_clean()."</tr>",array("goto"=>true));
+      echo operation_line($operation);
     }
   ?>
 </tbody>
@@ -49,12 +44,12 @@
 <tbody>
   <tr class="total">
     <td colspan="3">Total</td>
-    <td><?php echo pretty_amount(sum_array($operations, "amount", "positive"),false); ?></td>
-    <td><?php echo pretty_amount(sum_array($operations, "amount", "negative"),false); ?></td>
+    <td><?php echo pretty_amount($sum_incomes,false); ?></td>
+    <td><?php echo pretty_amount($sum_spendings,false); ?></td>
   </tr>
   <tr class="total">
     <td colspan="3">Solde</td>
-    <td colspan="2"><b><?php echo pretty_amount(sum_array($operations, "amount")); ?></b></td>
+    <td colspan="2"><b><?php echo pretty_amount($balance); ?></b></td>
   </tr>
 </tbody>
 <thead class="separator">
@@ -69,30 +64,24 @@
     </td>
   </tr>
   <?php foreach($op_pending_kes_validations as $operation ){
-      ob_start();
-      ?>
-      <td class="element_name">
-        <?php echo $operation["comment"]; ?>
-      </td>
-      <td class="tags">
-        <?php echo pretty_tags(select_tags_operation($operation["id"]), true); ?>
-      </td>
-      <td>
-        <?php echo pretty_date($operation["date"]); ?>
-      </td>
-      <?php if ($operation["amount"] < 0) {
-        ?><td></td><td>
-          <?php echo pretty_amount($operation["amount"],false); ?>
-        </td><?php
-      } else {
-        ?><td>
-          <?php echo pretty_amount($operation["amount"],false); ?>
-        </td><td></td>
-        <?php
-      }
-      echo link_to(path("show", "operation", $operation["id"], binet_prefix($operation["binet"], $operation["term"])),
-      "<tr>".ob_get_clean()."</tr>",array("goto"=>true));
+      echo operation_line($operation);
     } ?>
+</tbody>
+<thead class="separator">
+  <tr>
+    <td colspan="5"></td>
+  </tr>
+</thead>
+<tbody>
+  <tr class="total">
+    <td colspan="3">Total après validation des opérations en attente</td>
+    <td><?php echo pretty_amount($sum_incomes + $sum_pending_incomes,false); ?></td>
+    <td><?php echo pretty_amount($sum_spendings + $sum_pending_spendings,false); ?></td>
+  </tr>
+  <tr class="total">
+    <td colspan="3">Solde après validation des opérations en attente</td>
+    <td colspan="2"><b><?php echo pretty_amount($balance + $pendings_balance); ?></b></td>
+  </tr>
 </tbody>
 <?php
   $table = ob_get_clean();
