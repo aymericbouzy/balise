9059cf7d02d5ad09c668bfbb6b870ba6e9c3f102
Newbienathou <nathan.eckert.x2012@gmail.com>
2015-03-04 12:32:49 +0100

    Texte aide binets subventionneur
    Demande de la date d’utilisation des subventions

--- htdocs/beta/view/help/wave/new.php
+++ htdocs/beta/view/help/wave/new.php
@@ -8,6 +8,7 @@
       Ces dates pourront être modifiées par la suite.<br>
    Il faut également que tu choisisses la question que tu veux poser aux binets.
    Par exemple, pourquoi êtes-vous éligible à cette vague, que compter vous faire de vos sous ...
+   Ou encore, à quelle date comptez-vous utilisez votre subventions, quand se déroule votre événement ...
    À toi d'y réflechir pour avoir les renseignements qui t'intéressent.
    Tu peux même poser plusieurs questions à la suite dans ce champ<br>
 
