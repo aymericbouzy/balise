96ab04f906c4ac3388cbefc87151ac2d0a2b2f1d
Newbienathou <nathan.eckert.x2012@gmail.com>
2015-03-04 18:23:29 +0100

    Création de l'aide pour de nouvelles pages

---
+++ htdocs/beta/view/help/binet/validation.php
@@ -0,0 +1,3 @@
+<h1>Voici la page des validations !</h1>
+<p>En cliquant sur une des demandes de validation, tu accéderas aux détails de la demande et pourra la valider
+</p>
---
+++ htdocs/beta/view/help/student/show.php
@@ -0,0 +1,2 @@
+<h1>Contact</h1>
+<p>Tu peux contacter cette personne ainsi que voir la liste des binets qu'elle administre et voir plus en détail ces binets</p>
