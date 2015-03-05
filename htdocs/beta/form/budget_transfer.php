bea1f828890b5ac79edbb4a54d2b3874b1652f7f
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-03-05 16:08:30 +0100

    created form

---
+++ htdocs/beta/form/budget_transfer.php
@@ -0,0 +1,25 @@
+<?php
+
+  $origin_action = "transfer";
+  $destination_action = "copy";
+  $form["redirect_to_if_error"] = path($origin_action, "budget", "", binet_prefix($GLOBALS["binet"], $GLOBALS["term"]));
+  $form["destination_path"] = path($destination_action, "budget", "", binet_prefix($GLOBALS["binet"], $GLOBALS["term"]));
+  $form["html_form_path"] = VIEW_PATH."binet/budget/transfer_form.php";
+
+  foreach (select_budgets(array("binet" => $binet, "term" => $term - 1)) as $budget) {
+    $budget = select_budget($budget["id"], array("id", "label"));
+    $form["fields"]["budget_".$budget["id"]] = create_boolean_field("le choix du budget \"".$budget["label"]."\"", array("optional" => 1));
+  }
+
+  function structured_review_maker($validated_input) {
+    $structured_input["budgets"] = array();
+    foreach ($validated_input as $name => $value) {
+      $matched_groups = array();
+      if (!is_empty($value) && preg_match("/^amount_([0-9]*)$/", $name, $matched_groups)) {
+        $structured_input["budgets"][] = $matched_groups[1];
+      }
+    }
+    return $structured_input;
+  }
+
+  $form["structured_input_maker"] = "structured_review_maker";
