7966bada33978813b2736053fe769c053afe2f89
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-03-05 13:14:32 +0100

    updated transfer_form view

--- htdocs/beta/view/binet/budget/budget_transfer.php
+++
@@ -1,7 +0,0 @@
-<form>
-<?php  foreach ($budgets as $budget) {
-      $budget = select_budget($budget,array("label","id"))
-      echo form_group_checkbox($budget["label"],"?",true, $form);
-  }
-  echo form_submit_button("Transférer les budgets"); ?>
-</form>
