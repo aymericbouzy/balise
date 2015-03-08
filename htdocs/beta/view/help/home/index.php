fd27bbc331a8028e0c9f878b25ad0a20c0e217b2
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-03-08 23:35:30 +0100

    corrections

--- htdocs/beta/view/help/home/index.php
+++ htdocs/beta/view/help/home/index.php
@@ -5,8 +5,6 @@
         <li>Accéder à des informations sur l'ensemble des binets du plateau (la liste ou encore l'historique des vagues de subventions)</li>
       </ul>
 			Durant ta navigation sur le site, tu pourras à tout moment revenir à cette page en cliquant en haut à gauche sur "Balise"<br>
-Il y a deux boutons que tu dois remarquer sur ce site : le bouton "Aide" et le bouton "Plus" en haut à droite.<br>
-L'aide varie en fonction des pages, n'hésite pas à t'y référer si tu as un souci ou une question.<br>
-Si tu as fais une dépense pour un binet, tu peux le lui signaler via le bouton "Plus", en lui proposant une opération.
-L'administrateur de ce binet validera ton opération ou non, par exemple après que tu lui as passé la facture.
+Il y a un bouton que tu dois remarquer sur ce site : le bouton "Aide".<br>
+L'aide varie en fonction des pages, n'hésite pas à t'y référer si tu as un souci ou une question.
 		</p>
