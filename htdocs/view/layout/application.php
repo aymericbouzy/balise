a3a9a52bc9f4ce756abaebc19ad02374a40649f0
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-03-09 23:55:41 +0100

    updated bug-report modal title

--- htdocs/view/layout/application.php
+++ htdocs/view/layout/application.php
@@ -92,7 +92,7 @@
       include LAYOUT_PATH."footer.php";
       ?>
     </footer>
-    <?php echo modal("bug-report", "Rapport de bug", get_html_form("bug_report"));?>
+    <?php echo modal("bug-report", "Contacter l'équipe du site Balise", get_html_form("bug_report"));?>
     <script src = "<?php echo ASSET_PATH; ?>js/common.js"></script>
   </body>
 </html>
