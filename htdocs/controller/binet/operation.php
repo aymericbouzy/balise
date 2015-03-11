8b23cd38b5d95e1ea33137c6a493831cc4d60038
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-03-11 15:41:33 +0100

    moved validation out of binet

--- htdocs/controller/binet/operation.php
+++ htdocs/controller/binet/operation.php
@@ -54,6 +54,7 @@
     foreach (select_operations(array_merge($query_array, array("binet" => $binet, "term" => $term, "state" => "waiting_validation")), "date",false) as $operation){
       $op_pending_kes_validations[] = select_operation($operation["id"], array("id", "comment", "amount", "date", "type","term","binet"));
     }
+    $pending_validations_operations = pending_validations_operations($binet, $term);
     break;
 
   case "new":
