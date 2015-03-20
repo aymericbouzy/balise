b9deb91b5e03d08c557694deed9a3506fc77512d
AlexandreGuinaudeau <alexandre.guinaudeau@polytechnique.edu>
2015-03-20 15:02:08 +0100

    configuration of development.php

---
+++ htdocs/config/example.php
@@ -0,0 +1,16 @@
+<?php
+
+  define("WEBMASTER_EMAIL", "aymeric.bouzy@polytechique.edu");
+  define("STATE", "development");
+
+  // when changing these values, run localhost/url_rewriting.php to rewrite .htaccess file.
+  define("URL_REWRITE", true); // set to false if you don't want pretty urls.
+  define("ROOT_PATH", ""); // set to "" to remove root_path
+
+  define("DATABASE_USERNAME", "root");
+  define("DATABASE_PASSWORD", "root");
+  define("DATABASE_NAME", "balise");
+  define("DATABASE_HOST", "localhost");
+  define("DATABASE_PORT", "8889");
+  define("FRANKIZ_AUTH_KEY", "A4d!fgr6?45GF8");
+  define("REAL_FRANKIZ_CONNECTION", false);
---
+++ htdocs/config/production.php
@@ -0,0 +1,16 @@
+<?php
+
+  define("WEBMASTER_EMAIL", "balise.bugreport@gmail.com");
+  define("STATE", "production");
+
+  // when changing these values, run localhost/url_rewriting.php to rewrite .htaccess file.
+  define("URL_REWRITE", true); // set to false if you don't want pretty urls.
+  define("ROOT_PATH", ""); // set to "" to remove root_path
+
+  define("DATABASE_USERNAME", "projetbalise");
+  define("DATABASE_PASSWORD", "j26lcdv]");
+  define("DATABASE_NAME", "projetbalise");
+  define("DATABASE_HOST", "localhost");
+  define("DATABASE_PORT", "");
+  define("FRANKIZ_AUTH_KEY", "7d7Uw5Q3p27NR4WrUixe");
+  define("REAL_FRANKIZ_CONNECTION", true);
