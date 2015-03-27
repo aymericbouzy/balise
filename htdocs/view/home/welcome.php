00a0c0afe0b2edb4ab0756757a9f773cf76c2d70
Newbienathou <nathan.eckert.x2012@gmail.com>
2015-03-26 22:53:52 -0400

    Insertion des vidéos
    Les vidéos sont sur ma dropbox, n’hésitez pas à aller les récupérer

--- htdocs/view/home/welcome.php
+++ htdocs/view/home/welcome.php
@@ -8,11 +8,67 @@
         <li>gérer la trésorerie de ton binet</li>
         <li>demander et gérer tes subventions</li>
       </ul>
-      Pour recevoir les droits d'administration de ton binet, <?php echo link_to("https://docs.google.com/spreadsheets/d/1haTwZQud3of6Tmc_5dGDUqnicxTtTQtt0UhJKWMAt3g/edit#gid=0", "inscris-toi à un créneau à la Kès"); ?>.
+      Pour recevoir les droits d'administration de ton binet, va faire ta passation à la Kès et demande les droits aux kessiers binets.
     </p>
     <p>
-      Si tu as des remarques ou des suggestions, n'hésite pas à nous en faire part à l'aide du bouton "Contact".
+      Si tu as des remarques ou des suggestions, n'hésite pas à nous en faire part à l'aide du bouton "Contact".</br>
+      N'hésite pas non plus à aller regarder les tutoriels vidéos si tu as un souci.
     </p>
+  </div>
+  <div id="video1" class="panel shadowed">
+    <div class="title">
+      Tutoriel : présentation générale du site
+    </div>
+    <video controls poster="../../video/presentation.png" width="75%">
+      <source src="../../video/presentation.mp4">
+      <source src="../../video/presentation.webm">
+      <source src="../../video/presentation.ogv">
+        Votre navigateur n'est pas à jour, il ne peut pas lire cette vidéo !
+    </video>
+  </div>
+  <div id="video2" class="panel shadowed">
+    <div class="title">
+      Tutoriel : mon binet et son budget
+    </div>
+    <video controls poster="../../video/monbinet.png" width="75%">
+      <source src="../../video/monbinet.mp4">
+      <source src="../../video/monbinet.webm">
+      <source src="../../video/monbinet.ogv">
+        Votre navigateur n'est pas à jour, il ne peut pas lire cette vidéo !
+    </video>
+  </div>
+  <div class="panel shadowed">
+    <div class="title">
+      Tutoriel : mon budget et mes opérations
+    </div>
+    <video controls poster="../../video/operation.png" width="75%">
+      <source src="../../video/operation.mp4">
+      <source src="../../video/operation.webm">
+      <source src="../../video/operation.ogv">
+        Votre navigateur n'est pas à jour, il ne peut pas lire cette vidéo !
+    </video>
+  </div>
+  <div class="panel shadowed">
+    <div class="title">
+      Tutoriel : qui a accès à ma trésorerie
+    </div>
+    <video controls poster="../../video/droits.png" width="75%">
+      <source src="../../video/droits.mp4">
+      <source src="../../video/droits.webm">
+      <source src="../../video/droits.ogv">
+        Votre navigateur n'est pas à jour, il ne peut pas lire cette vidéo !
+    </video>
+  </div>
+  <div class="panel shadowed">
+    <div class="title">
+      Tutoriel : faire ma demande de subventions
+    </div>
+    <video controls poster="../../video/subvention.png" width="75%">
+      <source src="../../video/subvention.mp4">
+      <source src="../../video/subvention.webm">
+      <source src="../../video/subvention.ogv">
+        Votre navigateur n'est pas à jour, il ne peut pas lire cette vidéo !
+    </video>
   </div>
 </div>
  <!-- /container -->
