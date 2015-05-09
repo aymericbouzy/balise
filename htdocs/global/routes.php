491a008f474a4fbbd695862128629fbd7b946746
victornicolet <victor.nicolet.0@gmail.com>
2015-05-09 11:14:49 +0200

    Correction over previous merge

--- htdocs/global/routes.php
+++ htdocs/global/routes.php
@@ -137,7 +137,6 @@
     write_controller_rules(array("controller" => "budget", "binet_prefix" => true, "action_on_collection" => array("transfer", "copy")));
     write_controller_rules(array("controller" => "operation", "binet_prefix" => true, "action_on_member" => array("validate", "review")));
     write_controller_rules(array("controller" => "request", "binet_prefix" => true, "action_on_member" => array("send", "review", "grant", "reject", "edit_converted_amount", "set_converted_amount", "send_back")));
-    write_controller_rules(array("controller" => "validation", "binet_prefix" => true, "except" => array("show", "edit", "update", "new", "create", "delete")));
     write_controller_rules(array("controller" => "wave", "binet_prefix" => true, "except" => array("delete"), "action_on_member" => array("publish", "open")));
 
     fclose($htaccess);
